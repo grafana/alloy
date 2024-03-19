13d4cd3d266b9a98b56b28714dd4113426386fa7
Robert Fratto <robertfratto@gmail.com>
2024-03-19 12:55:40 -0400

    tools/gen-versioned-files: move version file to root

--- tools/gen-versioned-files/gen-versioned-files.sh
+++ tools/gen-versioned-files/gen-versioned-files.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
-AGENT_VERSION=$(cat ./tools/gen-versioned-files/agent-version.txt | tr -d '\n')
+ALLOY_VERSION=$(sed -e '/^#/d' -e '/^$/d' VERSION | tr -d '\n')
 
-if [ -z "$AGENT_VERSION" ]; then
-    echo "AGENT_VERSION can't be found. Are you running this from the repo root?"
+if [ -z "$ALLOY_VERSION" ]; then
+    echo "ALLOY_VERSION can't be found. Are you running this from the repo root?"
     exit 1
 fi
 
-versionMatcher='^v[0-9]+\.[0-9]+\.[0-9]+(-rc\.[0-9]+)?$'
+versionMatcher='^v[0-9]+\.[0-9]+\.[0-9]$'
 
-if ! echo "$AGENT_VERSION" | grep -Eq "$versionMatcher"; then
-    echo "AGENT_VERSION env var is not in the correct format. It should be in the format of vX.Y.Z or vX.Y.Z-rc.N"
+if ! echo "$ALLOY_VERSION" | grep -Eq "$versionMatcher"; then
+    echo "ALLOY_VERSION env var is not in the correct format. It should be in the format of vX.Y.Z"
     exit 1
 fi
 
 templates=$(find . -type f -name "*.t" -not -path "./.git/*")
 for template in $templates; do
     echo "Generating ${template%.t}"
-    sed -e "s/\$AGENT_VERSION/$AGENT_VERSION/g" < "$template" > "${template%.t}"
+    sed -e "s/\$ALLOY_VERSION/$ALLOY_VERSION/g" < "$template" > "${template%.t}"
 done
