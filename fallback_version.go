a549272142853dd9484bca63b5f8dbf1ae28910b
Robert Fratto <robertfratto@gmail.com>
2024-04-19 11:58:53 -0400

    main: use VERSION file as fallback for version reporting
    Currently, if Alloy was built without setting build information, it
    reports its version as "v0.0.0".
    
    This commit adds using the VERSION file as fallback, so if the version
    information wasn't present (such as when ussing `go run .`), the
    reported version is taken from the VERSION file at the root of the
    repository.
    
    The version reported is then appended with `-devel` to indicate that it
    is not the final release.
    
    For example, as the VERSION file currently contains `v1.1.0`, then the
    fallback reported version will be `v1.1.0-devel`.

---
+++ fallback_version.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	_ "embed"
+	"strings"
+
+	"github.com/grafana/alloy/internal/build"
+)
+
+//go:embed VERSION
+var fallbackVersionText []byte
+
+// fallbackVersion returns a version string to use for when the version isn't
+// explicitly set at build time. The version string will always have -devel
+// appended to it.
+func fallbackVersion() string {
+	return fallbackVersionFromText(fallbackVersionText)
+}
+
+func fallbackVersionFromText(text []byte) string {
+	// Find the first line in fallbackVersionText which isn't a blank line or a
+	// line starting with #.
+	scanner := bufio.NewScanner(bytes.NewReader(text))
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		if len(line) == 0 || strings.HasPrefix(line, "#") {
+			continue
+		}
+
+		return line + "-devel"
+	}
+
+	// We shouldn't hit this case since we always control the contents of the
+	// VERSION file, but just in case we'll return the existing version.
+	return build.Version
+}
