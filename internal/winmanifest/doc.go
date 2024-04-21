209ab4f822f7c7a606e4edea3dc4c990f29a16ce
Jan-Otto Kröpke <mail@jkroepke.de>
2024-04-21 16:45:23 +0200

    Add Windows application manifest
    Signed-off-by: Jan-Otto Kröpke <mail@jkroepke.de>

---
+++ internal/winmanifest/doc.go
@@ -0,0 +1,9 @@
+// Package winmanifest provides a basic manifest.
+//
+// You import it for its side effects only, as
+//
+//	import _ "github.com/grafana/alloy/internal/winmanifest"
+//
+// On non-Windows platforms this package does nothing.
+
+package winmanifest
