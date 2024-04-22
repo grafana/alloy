ef9ee63ae32710cbda232a547e2a191d7a26551f
Robert Fratto <robertfratto@gmail.com>
2024-04-22 11:58:44 -0400

    misc: switch to grafana/alloy-build-image (#633)

--- tools/make/build-container.mk
+++ tools/make/build-container.mk
@@ -34,8 +34,8 @@
 # variable names should be passed through to the container.
 
 USE_CONTAINER       ?= 0
-BUILD_IMAGE_VERSION ?= 0.40.2
-BUILD_IMAGE         ?= grafana/agent-build-image:$(BUILD_IMAGE_VERSION)
+BUILD_IMAGE_VERSION ?= v0.1.0
+BUILD_IMAGE         ?= grafana/alloy-build-image:$(BUILD_IMAGE_VERSION)
 DOCKER_OPTS         ?= -it
 
 #
