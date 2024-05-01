c2c4eb92f7c88715755ac93d1f0e82b1f684bf20
Jenna M <jenna@polyrain.dev>
2024-05-02 08:20:28 +1000

    More dd types, pulling more in from upstream

--- internal/component/otelcol/config_datadog.go
+++ internal/component/otelcol/config_datadog.go
@@ -1,6 +1,9 @@
 package otelcol
 
 import (
+	"time"
+
+	"github.com/open-telemetry/opentelemetry-collector-contrib/exporter/datadogexporter"
 	"go.opentelemetry.io/collector/config/confignet"
 	"go.opentelemetry.io/collector/config/configopaque"
 )
@@ -10,31 +13,31 @@ import (
 type TracesConfig struct {
 	confignet.TCPAddrConfig `mapstructure:",squash"`
 
-	IgnoreResources []string `mapstructure:"ignore_resources"`
+	IgnoreResources []string `alloy:"ignore_resources"`
 
-	SpanNameRemappings map[string]string `mapstructure:"span_name_remappings"`
+	SpanNameRemappings map[string]string `alloy:"span_name_remappings"`
 
-	SpanNameAsResourceName bool `mapstructure:"span_name_as_resource_name"`
+	SpanNameAsResourceName bool `alloy:"span_name_as_resource_name"`
 
-	ComputeStatsBySpanKind bool `mapstructure:"compute_stats_by_span_kind"`
+	ComputeStatsBySpanKind bool `alloy:"compute_stats_by_span_kind"`
 
-	PeerServiceAggregation bool `mapstructure:"peer_service_aggregation"`
+	PeerServiceAggregation bool `alloy:"peer_service_aggregation"`
 
-	PeerTagsAggregation bool `mapstructure:"peer_tags_aggregation"`
+	PeerTagsAggregation bool `alloy:"peer_tags_aggregation"`
 
-	PeerTags    []string `mapstructure:"peer_tags"`
-	TraceBuffer int      `mapstructure:"trace_buffer"`
+	PeerTags    []string `alloy:"peer_tags"`
+	TraceBuffer int      `alloy:"trace_buffer"`
 }
 
 // MetricsExporterConfig holds the configuration settings for the Datadog metrics exporter
 type MetricsConfig struct {
 	// DeltaTTL defines the time that previous points of a cumulative monotonic
 	// metric are kept in memory to calculate deltas
-	DeltaTTL int64 `mapstructure:"delta_ttl"`
+	DeltaTTL int64 `alloy:"delta_ttl"`
 
 	// TCPAddr.Endpoint is the host of the Datadog intake server to send metrics to.
 	// If unset, the value is obtained from the Site.
-	confignet.TCPAddrConfig `mapstructure:",squash"`
+	confignet.TCPAddrConfig `alloy:"block,squash"`
 
 	ExporterConfig MetricsExporterConfig `mapstructure:",squash"`
 
@@ -49,33 +52,43 @@ type MetricsConfig struct {
 }
 
 type MetricsExporterConfig struct {
-	// ResourceAttributesAsTags, if set to true, will use the exporterhelper feature to transform all
-	// resource attributes into metric labels, which are then converted into tags
 	ResourceAttributesAsTags bool `mapstructure:"resource_attributes_as_tags"`
 
-	// InstrumentationScopeMetadataAsTags, if set to true, adds the name and version of the
-	// instrumentation scope that created a metric to the metric tags
 	InstrumentationScopeMetadataAsTags bool `mapstructure:"instrumentation_scope_metadata_as_tags"`
 }
 
 type HistogramConfig struct {
-	// Mode for exporting histograms. Valid values are 'distributions', 'counters' or 'nobuckets'.
-	//  - 'distributions' sends histograms as Datadog distributions (recommended).
-	//  - 'counters' sends histograms as Datadog counts, one metric per bucket.
-	//  - 'nobuckets' sends no bucket histogram metrics. Aggregation metrics will still be sent
-	//    if `send_aggregation_metrics` is enabled.
-	//
-	// The current default is 'distributions'.
-	Mode HistogramMode `mapstructure:"mode"`
+	Mode             datadogexporter.HistogramMode `alloy:"mode"`
+	SendCountSum     bool                          `alloy:"send_count_sum_metrics"`
+	SendAggregations bool                          `alloy:"send_aggregation_metrics"`
+}
 
-	// SendCountSum states if the export should send .sum and .count metrics for histograms.
-	// The default is false.
-	// Deprecated: [v0.75.0] Use `send_aggregation_metrics` (HistogramConfig.SendAggregations) instead.
-	SendCountSum bool `mapstructure:"send_count_sum_metrics"`
+type SumConfig struct {
+	CumulativeMonotonicMode datadogexporter.CumulativeMonotonicSumMode `mapstructure:"cumulative_monotonic_mode"`
 
-	// SendAggregations states if the exporter should send .sum, .count, .min and .max metrics for histograms.
-	// The default is false.
-	SendAggregations bool `mapstructure:"send_aggregation_metrics"`
+	InitialCumulativeMonotonicMode datadogexporter.InitialValueMode `mapstructure:"initial_cumulative_monotonic_value"`
+}
+
+type SummaryConfig struct {
+	Mode datadogexporter.SummaryMode `mapstructure:"mode"`
+}
+
+type TCPAddrConfig struct {
+	// Endpoint configures the address for this network connection.
+	// The address has the form "host:port". The host must be a literal IP address, or a host name that can be
+	// resolved to IP addresses. The port must be a literal port number or a service name.
+	// If the host is a literal IPv6 address it must be enclosed in square brackets, as in "[2001:db8::1]:80" or
+	// "[fe80::1%zone]:80". The zone specifies the scope of the literal IPv6 address as defined in RFC 4007.
+	Endpoint string `alloy:"endpoint"`
+
+	// DialerConfig contains options for connecting to an address.
+	DialerConfig DialerConfig `alloy:"dialer"`
+}
+
+type DialerConfig struct {
+	// Timeout is the maximum amount of time a dial will wait for
+	// a connect to complete. The default is no timeout.
+	Timeout time.Duration `alloy:"timeout"`
 }
 
 // DatadogAPISettings holds the configuration settings for the Datadog API.
