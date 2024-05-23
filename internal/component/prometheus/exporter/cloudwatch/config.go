947b23ba82a45f92326532dc2876f695eada193e
andriikushch <andrii.kushch@grafana.com>
2024-05-23 10:04:28 +0200

    Implements comments from the PR

--- internal/component/prometheus/exporter/cloudwatch/config.go
+++ internal/component/prometheus/exporter/cloudwatch/config.go
@@ -28,7 +28,7 @@ var defaults = Arguments{
 		Enabled:        false,
 		ScrapeInterval: 5 * time.Minute,
 	},
-	AWSSDKVersion: "v2",
+	UseAWSSDKVersionV2: false,
 }
 
 // Arguments are the Alloy based options to configure the embedded CloudWatch exporter.
@@ -40,7 +40,7 @@ type Arguments struct {
 	Discovery             []DiscoveryJob        `alloy:"discovery,block,optional"`
 	Static                []StaticJob           `alloy:"static,block,optional"`
 	DecoupledScrape       DecoupledScrapeConfig `alloy:"decoupled_scraping,block,optional"`
-	AWSSDKVersion         string                `alloy:"aws_sdk_version,attr,optional"`
+	UseAWSSDKVersionV2    bool                  `alloy:"aws_sdk_version_v2,attr,optional"`
 }
 
 // DecoupledScrapeConfig is the configuration for decoupled scraping feature.
