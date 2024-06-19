d82b33bc6102e51da4561834e903edb1a709c02f
Toon Tijtgat <toon.tijtgat@fluxys.com>
2024-06-19 08:11:25 +0200

    no array for of labelsets

--- internal/component/prometheus/operator/configgen/config_gen_probe_test.go
+++ internal/component/prometheus/operator/configgen/config_gen_probe_test.go
@@ -182,7 +182,7 @@ func TestGenerateProbeConfig(t *testing.T) {
 								{"__address__": "prometheus.io"},
 								{"__address__": "promcon.io"},
 							},
-							Labels: []model.LabelSet{
+							Labels: model.LabelSet{
 								{"static": "label"},
 								{"namespace": "default"},
 							},
