5dab44e0147edcc0a9231a5ddb4c0cde8d9eb68d
matt durham <mattdurham@ppog.org>
2024-06-21 09:35:11 -0400

    Fix 546 by using the upstream WAL.

--- internal/service/http/gatherer_translator.go
+++ internal/service/http/gatherer_translator.go
@@ -10,8 +10,10 @@ type translator struct {
 }
 
 var (
-	activeSeries  = "prometheus_remote_write_wal_storage_active_series"
-	deletedSeries = "prometheus_remote_write_wal_storage_deleted_series"
+	activeSeries       = "prometheus_remote_write_wal_storage_active_series"
+	deletedSeries      = "prometheus_remote_write_wal_storage_deleted_series"
+	totalCreatedSeries = "prometheus_remote_write_wal_storage_created_series_total"
+	appendedSamples    = "prometheus_remote_write_wal_samples_appended_total"
 )
 
 func (t translator) Gather() ([]*dto.MetricFamily, error) {
@@ -25,6 +27,10 @@ func (t translator) Gather() ([]*dto.MetricFamily, error) {
 			m.Name = &activeSeries
 		case "prometheus_agent_deleted_series":
 			m.Name = &deletedSeries
+		case "prometheus_agent_out_of_order_samples_total":
+			m.Name = &totalCreatedSeries
+		case "prometheus_agent_samples_appended_total":
+			m.Name = &appendedSamples
 		}
 	}
 	return metrics, nil
