7fa4b0b934aae285b5e1398522b57fa74271d892
Piotr <17101802+thampiotr@users.noreply.github.com>
2024-07-31 16:18:52 +0100

    Revert "Tests and foundations for new cluster peer discovery (#1311)" (#1390)
    This reverts commit 020713c2b63cfd4957b45ff8fe31d69cba89b1c9.

--- internal/alloycli/cluster_builder.go
+++ internal/alloycli/cluster_builder.go
@@ -18,7 +18,6 @@ import (
 
 	"github.com/grafana/alloy/internal/runtime/logging/level"
 	"github.com/grafana/alloy/internal/service/cluster"
-	"github.com/grafana/alloy/internal/service/cluster/discovery"
 )
 
 type clusterOptions struct {
@@ -37,7 +36,6 @@ type clusterOptions struct {
 	ClusterMaxJoinPeers       int
 	ClusterName               string
 	EnableStateUpdatesLimiter bool
-	EnableDiscoveryV2         bool
 }
 
 func buildClusterService(opts clusterOptions) (*cluster.Service, error) {
@@ -70,39 +68,24 @@ func buildClusterService(opts clusterOptions) (*cluster.Service, error) {
 		return nil, err
 	}
 
-	// New, refactored and improved peer discovery.
-	// TODO(alloy/#1274): Remove the old peer discovery code once this becomes default.
-	if opts.EnableDiscoveryV2 {
-		config.DiscoverPeers, err = discovery.NewPeerDiscoveryFn(discovery.Options{
-			JoinPeers:     opts.JoinPeers,
-			DiscoverPeers: opts.DiscoverPeers,
-			DefaultPort:   listenPort,
-			Logger:        opts.Log,
-			Tracer:        opts.Tracer,
-		})
+	switch {
+	case len(opts.JoinPeers) > 0 && opts.DiscoverPeers != "":
+		return nil, fmt.Errorf("at most one of join peers and discover peers may be set")
+
+	case len(opts.JoinPeers) > 0:
+		config.DiscoverPeers = newStaticDiscovery(opts.JoinPeers, listenPort, opts.Log)
+
+	case opts.DiscoverPeers != "":
+		discoverFunc, err := newDynamicDiscovery(config.Log, opts.DiscoverPeers, listenPort)
 		if err != nil {
 			return nil, err
 		}
-	} else {
-		switch {
-		case len(opts.JoinPeers) > 0 && opts.DiscoverPeers != "":
-			return nil, fmt.Errorf("at most one of join peers and discover peers may be set")
+		config.DiscoverPeers = discoverFunc
 
-		case len(opts.JoinPeers) > 0:
-			config.DiscoverPeers = newStaticDiscovery(opts.JoinPeers, listenPort, opts.Log)
-
-		case opts.DiscoverPeers != "":
-			discoverFunc, err := newDynamicDiscovery(config.Log, opts.DiscoverPeers, listenPort)
-			if err != nil {
-				return nil, err
-			}
-			config.DiscoverPeers = discoverFunc
-
-		default:
-			// Here, both JoinPeers and DiscoverPeers are empty. This is desirable when
-			// starting a seed node that other nodes connect to, so we don't require
-			// one of the fields to be set.
-		}
+	default:
+		// Here, both JoinPeers and DiscoverPeers are empty. This is desirable when
+		// starting a seed node that other nodes connect to, so we don't require
+		// one of the fields to be set.
 	}
 
 	return cluster.New(config)
@@ -158,7 +141,7 @@ func appendDefaultPort(addr string, port int) string {
 
 type discoverFunc func() ([]string, error)
 
-func newStaticDiscovery(providedAddr []string, defaultPort int, log log.Logger) discovery.DiscoverFn {
+func newStaticDiscovery(providedAddr []string, defaultPort int, log log.Logger) discoverFunc {
 	return func() ([]string, error) {
 		addresses, err := buildJoinAddresses(providedAddr, log)
 		if err != nil {
@@ -222,7 +205,7 @@ func buildJoinAddresses(providedAddr []string, log log.Logger) ([]string, error)
 	return result, nil
 }
 
-func newDynamicDiscovery(l log.Logger, config string, defaultPort int) (discovery.DiscoverFn, error) {
+func newDynamicDiscovery(l log.Logger, config string, defaultPort int) (discoverFunc, error) {
 	providers := make(map[string]discover.Provider, len(discover.Providers)+1)
 	for k, v := range discover.Providers {
 		providers[k] = v
