0dfd3f5943869c98122bb20770942317218eaf72
Paschalis Tsilias <paschalist0@gmail.com>
2024-08-14 15:04:34 +0300

    Pass remotecfg's service.Host to the UI service
    Signed-off-by: Paschalis Tsilias <paschalist0@gmail.com>

--- internal/web/api/api.go
+++ internal/web/api/api.go
@@ -24,12 +24,13 @@ import (
 // AlloyAPI is a wrapper around the component API.
 type AlloyAPI struct {
 	alloy           service.Host
+	remotecfg       service.Host
 	CallbackManager livedebugging.CallbackManager
 }
 
 // NewAlloyAPI instantiates a new Alloy API.
-func NewAlloyAPI(alloy service.Host, CallbackManager livedebugging.CallbackManager) *AlloyAPI {
-	return &AlloyAPI{alloy: alloy, CallbackManager: CallbackManager}
+func NewAlloyAPI(alloy, remotecfg service.Host, CallbackManager livedebugging.CallbackManager) *AlloyAPI {
+	return &AlloyAPI{alloy: alloy, remotecfg: remotecfg, CallbackManager: CallbackManager}
 }
 
 // RegisterRoutes registers all the API's routes.
@@ -39,8 +40,14 @@ func (a *AlloyAPI) RegisterRoutes(urlPrefix string, r *mux.Router) {
 	// component IDs.
 
 	r.Handle(path.Join(urlPrefix, "/modules/{moduleID:.+}/components"), httputil.CompressionHandler{Handler: a.listComponentsHandler()})
+	r.Handle(path.Join(urlPrefix, "/remotecfg/modules/{moduleID:.+}/components"), httputil.CompressionHandler{Handler: a.listRemotecfgComponentsHandler()})
+
 	r.Handle(path.Join(urlPrefix, "/components"), httputil.CompressionHandler{Handler: a.listComponentsHandler()})
+	r.Handle(path.Join(urlPrefix, "/remotecfg/components"), httputil.CompressionHandler{Handler: a.listRemotecfgComponentsHandler()})
+
 	r.Handle(path.Join(urlPrefix, "/components/{id:.+}"), httputil.CompressionHandler{Handler: a.getComponentHandler()})
+	r.Handle(path.Join(urlPrefix, "/remotecfg/components/{id:.+}"), httputil.CompressionHandler{Handler: a.getRemotecfgComponentHandler()})
+
 	r.Handle(path.Join(urlPrefix, "/peers"), httputil.CompressionHandler{Handler: a.getClusteringPeersHandler()})
 	r.Handle(path.Join(urlPrefix, "/debug/{id:.+}"), a.liveDebugging())
 }
@@ -71,12 +78,63 @@ func (a *AlloyAPI) listComponentsHandler() http.HandlerFunc {
 	}
 }
 
+func (a *AlloyAPI) listRemotecfgComponentsHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// moduleID is set from the /modules/{moduleID:.+}/components route above
+		// but not from the /components route.
+		var moduleID string
+		if vars := mux.Vars(r); vars != nil {
+			moduleID = vars["moduleID"]
+		}
+
+		components, err := a.remotecfg.ListComponents(moduleID, component.InfoOptions{
+			GetHealth: true,
+		})
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		bb, err := json.Marshal(components)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		_, _ = w.Write(bb)
+	}
+}
+
 func (a *AlloyAPI) getComponentHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		vars := mux.Vars(r)
 		requestedComponent := component.ParseID(vars["id"])
 
 		component, err := a.alloy.GetComponent(requestedComponent, component.InfoOptions{
+			GetHealth:    true,
+			GetArguments: true,
+			GetExports:   true,
+			GetDebugInfo: true,
+		})
+		if err != nil {
+			http.NotFound(w, r)
+			return
+		}
+
+		bb, err := json.Marshal(component)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		_, _ = w.Write(bb)
+	}
+}
+
+func (a *AlloyAPI) getRemotecfgComponentHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		requestedComponent := component.ParseID(vars["id"])
+
+		component, err := a.remotecfg.GetComponent(requestedComponent, component.InfoOptions{
 			GetHealth:    true,
 			GetArguments: true,
 			GetExports:   true,
