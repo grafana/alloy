0d588a48c217acbad62480c066bdeb8d77413fd1
William Dumont <william.dumont@grafana.com>
2024-08-27 10:58:56 +0200

    fix stub component

--- internal/component/otelcol/exporter/datadog/datadog_stub.go
+++ internal/component/otelcol/exporter/datadog/datadog_stub.go
@@ -2,11 +2,18 @@
 
 package datadog
 
+import (
+	"context"
+
+	"github.com/grafana/alloy/internal/component"
+	"github.com/grafana/alloy/internal/runtime/logging/level"
+)
+
 func init() {
 	component.Register(component.Registration{
 		Name:      "otelcol.exporter.datadog",
 		Community: true,
-		Args:      Arguments{},
+		Args:      Argument{},
 
 		Build: func(opts component.Options, args component.Arguments) (component.Component, error) {
 			level.Info(opts.Logger).Log("msg", "otelcol.exporter.datadog is unsupported on freebsd")
@@ -20,8 +27,9 @@ var (
 )
 
 // FakeComponent implements the otelcol.exporter.datadog component for freebsd environments.
-type FakeComponent struct {
-}
+type FakeComponent struct{}
+
+type Argument struct{}
 
 func (f *FakeComponent) Run(ctx context.Context) error {
 	<-ctx.Done()
