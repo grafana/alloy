af04c73da5a07e0971cfc81175134678a484c36e
Romain Gaillard <rgaillard@gmail.com>
2024-10-14 13:04:36 +0200

    fix: Disable generic API key rule by default in loki.secretfilter (#1877)
    * Disable generic api key rule by default
    
    * Update doc
    
    * Update docs/sources/reference/components/loki/loki.secretfilter.md
    
    Co-authored-by: Clayton Cornell <131809008+clayton-cornell@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Clayton Cornell <131809008+clayton-cornell@users.noreply.github.com>

--- internal/component/loki/secretfilter/secretfilter.go
+++ internal/component/loki/secretfilter/secretfilter.go
@@ -53,7 +53,7 @@ type Arguments struct {
 	GitleaksConfig string              `alloy:"gitleaks_config,attr,optional"` // Path to the custom gitleaks.toml file. If empty, the embedded one is used
 	Types          []string            `alloy:"types,attr,optional"`           // Types of secret to look for (e.g. "aws", "gcp", ...). If empty, all types are included
 	RedactWith     string              `alloy:"redact_with,attr,optional"`     // Redact the secret with this string. Use $SECRET_NAME and $SECRET_HASH to include the secret name and hash
-	ExcludeGeneric bool                `alloy:"exclude_generic,attr,optional"` // Exclude the generic API key rule (default: false)
+	IncludeGeneric bool                `alloy:"include_generic,attr,optional"` // Include the generic API key rule (default: false)
 	AllowList      []string            `alloy:"allowlist,attr,optional"`       // List of regexes to allowlist (on top of what's in the Gitleaks config)
 	PartialMask    uint                `alloy:"partial_mask,attr,optional"`    // Show the first N characters of the secret (default: 0)
 }
@@ -338,7 +338,7 @@ func (c *Component) Update(args component.Arguments) error {
 	}
 
 	// Add the generic API key rule last if needed
-	if ruleGenericApiKey != nil && !c.args.ExcludeGeneric {
+	if ruleGenericApiKey != nil && c.args.IncludeGeneric {
 		c.Rules = append(c.Rules, *ruleGenericApiKey)
 	}
 
