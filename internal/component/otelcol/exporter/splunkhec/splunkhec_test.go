a48325fa4532bba2e8374104fca574aea8882190
Adlotsof <trsonn@googlemail.com>
2024-10-21 09:33:35 +0200

    fix test type

--- internal/component/otelcol/exporter/splunkhec/splunkhec_test.go
+++ internal/component/otelcol/exporter/splunkhec/splunkhec_test.go
@@ -4,19 +4,179 @@ import (
 	"testing"
 	"time"
 
+	"github.com/open-telemetry/opentelemetry-collector-contrib/exporter/splunkhecexporter"
+
 	"github.com/grafana/alloy/internal/component/otelcol/exporter/splunkhec"
-	splunkhec_config "github.com/grafana/alloy/internal/component/otelcol/exporter/splunkhec/config"
 	"github.com/grafana/alloy/syntax"
 	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/collector/config/configauth"
+	"go.opentelemetry.io/collector/config/confighttp"
+	"go.opentelemetry.io/collector/config/configopaque"
 	"go.opentelemetry.io/collector/config/configretry"
+	"go.opentelemetry.io/collector/config/configtls"
+	"go.opentelemetry.io/collector/exporter/exporterbatcher"
 	"go.opentelemetry.io/collector/exporter/exporterhelper"
 )
 
 func TestConfigConversion(t *testing.T) {
+	expectedCustomise := splunkhecexporter.Config{
+		ClientConfig: confighttp.ClientConfig{
+			Endpoint: "http://localhost:8088", ProxyURL: "",
+			TLSSetting: configtls.ClientConfig{
+				Config: configtls.Config{
+					CAFile:                   "",
+					CAPem:                    "",
+					IncludeSystemCACertsPool: false,
+					CertFile:                 "",
+					CertPem:                  "",
+					KeyFile:                  "",
+					KeyPem:                   "",
+					MinVersion:               "",
+					MaxVersion:               "",
+					CipherSuites:             []string(nil), ReloadInterval: 0,
+				},
+				Insecure:           false,
+				InsecureSkipVerify: true,
+				ServerName:         "",
+			},
+			ReadBufferSize:       0,
+			WriteBufferSize:      0,
+			Timeout:              10000000000,
+			Headers:              map[string]configopaque.String(nil),
+			Auth:                 (*configauth.Authentication)(nil),
+			Compression:          "",
+			MaxIdleConns:         (*int)(nil),
+			MaxIdleConnsPerHost:  (*int)(nil),
+			MaxConnsPerHost:      (*int)(nil),
+			IdleConnTimeout:      (*time.Duration)(nil),
+			DisableKeepAlives:    false,
+			HTTP2ReadIdleTimeout: 0,
+			HTTP2PingTimeout:     0,
+			Cookies:              (*confighttp.CookiesConfig)(nil),
+		},
+		QueueSettings: exporterhelper.QueueSettings{
+			Enabled:      true,
+			NumConsumers: 10,
+			QueueSize:    1000,
+			StorageID:    nil,
+		},
+		BackOffConfig: configretry.BackOffConfig{
+			Enabled:             true,
+			InitialInterval:     5000000000,
+			RandomizationFactor: 0.5,
+			Multiplier:          1.5,
+			MaxInterval:         30000000000,
+			MaxElapsedTime:      300000000000,
+		},
+		BatcherConfig: exporterbatcher.Config{
+			Enabled:       false,
+			FlushTimeout:  200000000,
+			MinSizeConfig: exporterbatcher.MinSizeConfig{MinSizeItems: 8192},
+			MaxSizeConfig: exporterbatcher.MaxSizeConfig{MaxSizeItems: 0},
+		},
+		LogDataEnabled:          true,
+		ProfilingDataEnabled:    true,
+		Token:                   "token",
+		Source:                  "source",
+		SourceType:              "sourcetype",
+		Index:                   "index",
+		DisableCompression:      false,
+		MaxContentLengthLogs:    0x200000,
+		MaxContentLengthMetrics: 0x200000,
+		MaxContentLengthTraces:  0x200000,
+		MaxEventSize:            0x500000,
+		SplunkAppName:           "Alloy",
+		SplunkAppVersion:        "",
+		HecFields:               splunkhecexporter.OtelToHecFields{SeverityText: "", SeverityNumber: ""},
+		HealthPath:              "/services/collector/health",
+		HecHealthCheckEnabled:   false,
+		ExportRaw:               false,
+		UseMultiMetricFormat:    false,
+		Heartbeat:               splunkhecexporter.HecHeartbeat{Interval: 0, Startup: false},
+		Telemetry: splunkhecexporter.HecTelemetry{
+			Enabled:              false,
+			OverrideMetricsNames: map[string]string(nil),
+			ExtraAttributes:      map[string]string(nil),
+		},
+	}
+
+	expectedMinimal := &splunkhecexporter.Config{
+		ClientConfig: confighttp.ClientConfig{
+			Endpoint: "http://localhost:8088",
+			ProxyURL: "",
+			TLSSetting: configtls.ClientConfig{
+				Config: configtls.Config{
+					CAFile:                   "",
+					CAPem:                    "",
+					IncludeSystemCACertsPool: false,
+					CertFile:                 "",
+					CertPem:                  "",
+					KeyFile:                  "",
+					KeyPem:                   "",
+					MinVersion:               "",
+					MaxVersion:               "",
+					CipherSuites:             []string(nil),
+					ReloadInterval:           0,
+				},
+				Insecure:           false,
+				InsecureSkipVerify: false,
+				ServerName:         "",
+			}, ReadBufferSize: 0,
+			WriteBufferSize: 0,
+			Timeout:         15000000000,
+			Headers:         map[string]configopaque.String(nil),
+			Auth:            (*configauth.Authentication)(nil),
+			Compression:     "", MaxIdleConns: (*int)(nil),
+			MaxIdleConnsPerHost:  (*int)(nil),
+			MaxConnsPerHost:      (*int)(nil),
+			IdleConnTimeout:      (*time.Duration)(nil),
+			DisableKeepAlives:    false,
+			HTTP2ReadIdleTimeout: 0,
+			HTTP2PingTimeout:     0,
+			Cookies:              (*confighttp.CookiesConfig)(nil)},
+		QueueSettings: exporterhelper.QueueSettings{
+			Enabled:      true,
+			NumConsumers: 10,
+			QueueSize:    1000,
+			StorageID:    (nil),
+		},
+		BackOffConfig: configretry.BackOffConfig{
+			Enabled:             true,
+			InitialInterval:     5000000000,
+			RandomizationFactor: 0.5,
+			Multiplier:          1.5,
+			MaxInterval:         30000000000,
+			MaxElapsedTime:      300000000000,
+		},
+		BatcherConfig: exporterbatcher.Config{Enabled: false,
+			FlushTimeout:  200000000,
+			MinSizeConfig: exporterbatcher.MinSizeConfig{MinSizeItems: 8192},
+			MaxSizeConfig: exporterbatcher.MaxSizeConfig{MaxSizeItems: 0}},
+		LogDataEnabled:       true,
+		ProfilingDataEnabled: true,
+		Token:                "token", Source: "",
+		SourceType: "", Index: "",
+		DisableCompression:      false,
+		MaxContentLengthLogs:    0x200000,
+		MaxContentLengthMetrics: 0x200000,
+		MaxContentLengthTraces:  0x200000,
+		MaxEventSize:            0x500000,
+		SplunkAppName:           "Alloy",
+		HecFields:               splunkhecexporter.OtelToHecFields{SeverityText: "", SeverityNumber: ""},
+		HealthPath:              "/services/collector/health", HecHealthCheckEnabled: false,
+		ExportRaw:            false,
+		UseMultiMetricFormat: false,
+		Heartbeat:            splunkhecexporter.HecHeartbeat{Interval: 0, Startup: false},
+		Telemetry: splunkhecexporter.HecTelemetry{
+			Enabled:              false,
+			OverrideMetricsNames: map[string]string(nil),
+			ExtraAttributes:      map[string]string(nil),
+		}}
+
 	tests := []struct {
 		testName string
 		alloyCfg string
-		expected splunkhec_config.SplunkHecArguments
+		expected *splunkhecexporter.Config
 	}{
 		{
 			testName: "full customise",
@@ -33,36 +193,7 @@ func TestConfigConversion(t *testing.T) {
 				   insecure_skip_verify = true
 		        }
 			`,
-			expected: splunkhec_config.SplunkHecArguments{
-				QueueSettings: exporterhelper.NewDefaultQueueSettings(),
-				RetrySettings: configretry.NewDefaultBackOffConfig(),
-				Splunk: splunkhec_config.SplunkConf{
-					Token:                   "token",
-					Source:                  "source",
-					SourceType:              "sourcetype",
-					Index:                   "index",
-					LogDataEnabled:          true,
-					ProfilingDataEnabled:    true,
-					DisableCompression:      false,
-					MaxContentLengthLogs:    2097152,
-					MaxContentLengthMetrics: 2097152,
-					MaxContentLengthTraces:  2097152,
-					MaxEventSize:            5242880,
-					SplunkAppName:           "Alloy",
-					SplunkAppVersion:        "",
-					HealthPath:              "/services/collector/health",
-					HecHealthCheckEnabled:   false,
-					ExportRaw:               false,
-					UseMultiMetricFormat:    false,
-					Heartbeat:               splunkhec_config.SplunkHecHeartbeat{},
-					Telemetry:               splunkhec_config.SplunkHecTelemetry{},
-				},
-				SplunkHecClientArguments: splunkhec_config.SplunkHecClientArguments{
-					Endpoint:           "http://localhost:8088",
-					Timeout:            10 * time.Second,
-					InsecureSkipVerify: true,
-				},
-			},
+			expected: &expectedCustomise,
 		},
 		{
 			testName: "minimal customise",
@@ -74,35 +205,7 @@ func TestConfigConversion(t *testing.T) {
 				  endpoint = "http://localhost:8088"
 				}
 				`,
-			expected: splunkhec_config.SplunkHecArguments{
-				QueueSettings: exporterhelper.NewDefaultQueueSettings(),
-				RetrySettings: configretry.NewDefaultBackOffConfig(),
-				Splunk: splunkhec_config.SplunkConf{
-					Token:                   "token",
-					Source:                  "",
-					SourceType:              "",
-					Index:                   "",
-					LogDataEnabled:          true,
-					ProfilingDataEnabled:    true,
-					DisableCompression:      false,
-					MaxContentLengthLogs:    2097152,
-					MaxContentLengthMetrics: 2097152,
-					MaxContentLengthTraces:  2097152,
-					MaxEventSize:            5242880,
-					SplunkAppName:           "Alloy",
-					SplunkAppVersion:        "",
-					HealthPath:              "/services/collector/health",
-					HecHealthCheckEnabled:   false,
-					ExportRaw:               false,
-					UseMultiMetricFormat:    false,
-					Heartbeat:               splunkhec_config.SplunkHecHeartbeat{},
-					Telemetry:               splunkhec_config.SplunkHecTelemetry{},
-				},
-				SplunkHecClientArguments: splunkhec_config.SplunkHecClientArguments{
-					Endpoint: "http://localhost:8088",
-					Timeout:  15 * time.Second,
-				},
-			},
+			expected: expectedMinimal,
 		},
 	}
 	for _, tt := range tests {
