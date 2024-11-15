368aab9b9e3dac0a5baad5b8df33d1eb47c1e057
Sam DeHaan <sam.dehaan@grafana.com>
2024-11-15 09:21:45 -0500

    Update names of metrics samples

--- internal/service/http/supportbundle.go
+++ internal/service/http/supportbundle.go
@@ -179,18 +179,18 @@ func ServeSupportBundle(rw http.ResponseWriter, b *Bundle, logsBuf *bytes.Buffer
 	rw.Header().Set("Content-Disposition", "attachment; filename=\"alloy-support-bundle.zip\"")
 
 	zipStructure := map[string][]byte{
-		"alloy-metadata.yaml":        b.meta,
-		"alloy-components.json":      b.components,
-		"alloy-peers.json":           b.peers,
-		"alloy-metrics-sample-1.txt": b.alloyMetricsStart,
-		"alloy-metrics-sample-2.txt": b.alloyMetricsEnd,
-		"alloy-runtime-flags.txt":    b.runtimeFlags,
-		"alloy-logs.txt":             logsBuf.Bytes(),
-		"pprof/cpu.pprof":            b.cpuBuf.Bytes(),
-		"pprof/heap.pprof":           b.heapBuf.Bytes(),
-		"pprof/goroutine.pprof":      b.goroutineBuf.Bytes(),
-		"pprof/mutex.pprof":          b.mutexBuf.Bytes(),
-		"pprof/block.pprof":          b.blockBuf.Bytes(),
+		"alloy-metadata.yaml":            b.meta,
+		"alloy-components.json":          b.components,
+		"alloy-peers.json":               b.peers,
+		"alloy-metrics-sample-start.txt": b.alloyMetricsStart,
+		"alloy-metrics-sample-end.txt":   b.alloyMetricsEnd,
+		"alloy-runtime-flags.txt":        b.runtimeFlags,
+		"alloy-logs.txt":                 logsBuf.Bytes(),
+		"pprof/cpu.pprof":                b.cpuBuf.Bytes(),
+		"pprof/heap.pprof":               b.heapBuf.Bytes(),
+		"pprof/goroutine.pprof":          b.goroutineBuf.Bytes(),
+		"pprof/mutex.pprof":              b.mutexBuf.Bytes(),
+		"pprof/block.pprof":              b.blockBuf.Bytes(),
 	}
 
 	for fn, b := range zipStructure {
