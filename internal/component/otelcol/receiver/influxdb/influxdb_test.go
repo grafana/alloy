55cedc471a12f06a197322a42057042db19c5d94
Eric Schmitt <eric.schmitt@grafana.com>
2024-11-25 13:30:27 -1000

    unrelated build failures - kick

--- internal/component/otelcol/receiver/influxdb/influxdb_test.go
+++ internal/component/otelcol/receiver/influxdb/influxdb_test.go
@@ -22,7 +22,6 @@ import (
     influxdbreceiver "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/influxdbreceiver"
 )
 
-
 type mockConsumer struct {
     lastMetricsConsumed pmetric.Metrics
 }
