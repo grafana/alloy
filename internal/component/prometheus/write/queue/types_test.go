75bf32202195f221d7898f4a113c632646d5b22f
matt durham <mattdurham@ppog.org>
2025-01-21 09:46:46 -0500

    remove bad test

--- internal/component/prometheus/write/queue/types_test.go
+++ internal/component/prometheus/write/queue/types_test.go
@@ -3,21 +3,8 @@ package queue
 import (
 	"github.com/grafana/alloy/syntax"
 	"github.com/stretchr/testify/require"
-	"strings"
 	"testing"
 )
-
-func TestBasicAuthAndTLSBothSetError(t *testing.T) {
-	args := defaultArgs()
-	args.Endpoints = make([]EndpointConfig, 1)
-	args.Endpoints[0] = defaultEndpointConfig()
-	args.Endpoints[0].Name = "test"
-	args.Endpoints[0].TLSConfig = &TLSConfig{}
-	args.Endpoints[0].BasicAuth = &BasicAuth{}
-	err := args.Validate()
-	require.Error(t, err)
-	require.True(t, strings.Contains(err.Error(), "cannot have both BasicAuth and TLSConfig"))
-}
 
 func TestParsingTLSConfig(t *testing.T) {
 	var args Arguments
