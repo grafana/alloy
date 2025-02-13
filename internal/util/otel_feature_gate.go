020b225f1abd14ef8f3bb88fe79e484fc7152b07
Sam DeHaan <sam.dehaan@grafana.com>
2025-02-13 01:28:45 -0500

    fix feature gate test

--- internal/util/otel_feature_gate.go
+++ internal/util/otel_feature_gate.go
@@ -7,6 +7,8 @@ import (
 
 	// Registers the "k8sattr.fieldExtractConfigRegex.disallow" feature gate.
 	_ "github.com/open-telemetry/opentelemetry-collector-contrib/processor/k8sattributesprocessor"
+	// Registers the "filelog.allowFileDeletion" feature gate.
+	_ "github.com/open-telemetry/opentelemetry-collector-contrib/pkg/stanza/fileconsumer"
 )
 
 type gateDetails struct {
@@ -26,11 +28,6 @@ var (
 		{
 			// This feature gate allows users of the otel filelogreceiver to use the `delete_after_read` setting.
 			name:    "filelog.allowFileDeletion",
-			enabled: true,
-		},
-		{
-			// This feature gate allows users of the otel filelogreceiver to use the `header` setting.
-			name:    "filelog.allowHeaderMetadataParsing",
 			enabled: true,
 		},
 	}
