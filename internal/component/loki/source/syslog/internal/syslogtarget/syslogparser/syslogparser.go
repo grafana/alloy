0b2eb41929467516ad61bed0ba14e03d7c72a524
Sam DeHaan <sam.dehaan@grafana.com>
2025-02-26 12:56:32 -0500

    Add comments about message framing in syslog parser

--- internal/component/loki/source/syslog/internal/syslogtarget/syslogparser/syslogparser.go
+++ internal/component/loki/source/syslog/internal/syslogtarget/syslogparser/syslogparser.go
@@ -38,12 +38,16 @@ func ParseStream(isRFC3164Message bool, useRFC3164DefaultYear bool, r io.Reader,
 		}
 	}
 
+	// See https://datatracker.ietf.org/doc/html/rfc6587 for details on message framing
+	// If a syslog message starts with '<' the first piece of the message is the priority, which means it must use
+	// an explicit framing character.
 	if b == '<' {
 		if isRFC3164Message {
 			nontransparent.NewParserRFC3164(syslog.WithListener(cb), syslog.WithMaxMessageLength(maxMessageLength), syslog.WithBestEffort()).Parse(buf)
 		} else {
 			nontransparent.NewParser(syslog.WithListener(cb), syslog.WithMaxMessageLength(maxMessageLength), syslog.WithBestEffort()).Parse(buf)
 		}
+		// If a syslog message starts with a digit, it must use octet counting, and the first piece of the message is the length
 	} else if b >= '0' && b <= '9' {
 		if isRFC3164Message {
 			octetcounting.NewParserRFC3164(syslog.WithListener(cb), syslog.WithMaxMessageLength(maxMessageLength), syslog.WithBestEffort()).Parse(buf)
