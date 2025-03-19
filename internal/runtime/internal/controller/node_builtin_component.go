53679e9e301cad19c1bf7428c1dad7c36b537de1
William Dumont <william.dumont@grafana.com>
2025-03-19 13:56:11 +0100

    [Feature branch] Live graph front-end (#2968)
    * graph front end
    
    * set relabelling count back
    
    * build data flow graph in backend
    
    * add todo in test
    
    * fix reflection

--- internal/runtime/internal/controller/node_builtin_component.go
+++ internal/runtime/internal/controller/node_builtin_component.go
@@ -120,6 +120,8 @@ type BuiltinComponentNode struct {
 
 	exportsMut sync.RWMutex
 	exports    component.Exports // Evaluated exports for the managed component
+
+	dataFlowEdgeRefs []string
 }
 
 var _ ComponentNode = (*BuiltinComponentNode)(nil)
@@ -168,6 +170,8 @@ func NewBuiltinComponentNode(globals ComponentGlobals, reg component.Registratio
 
 		evalHealth: initHealth,
 		runHealth:  initHealth,
+
+		dataFlowEdgeRefs: []string{},
 	}
 	cn.managedOpts = getManagedOptions(globals, cn)
 
@@ -458,4 +462,16 @@ func (cn *BuiltinComponentNode) setRunHealth(t component.HealthType, msg string)
 // managing.
 func (cn *BuiltinComponentNode) ModuleIDs() []string {
 	return cn.moduleController.ModuleIDs()
+}
+
+func (cn *BuiltinComponentNode) AddDataFlowEdgeTo(nodeID string) {
+	cn.dataFlowEdgeRefs = append(cn.dataFlowEdgeRefs, nodeID)
+}
+
+func (cn *BuiltinComponentNode) GetDataFlowEdgesTo() []string {
+	return cn.dataFlowEdgeRefs
+}
+
+func (cn *BuiltinComponentNode) ResetDataFlowEdgeTo() {
+	cn.dataFlowEdgeRefs = []string{}
 }
