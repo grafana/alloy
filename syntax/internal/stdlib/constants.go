2518fb91663412b54c373ed38a0815f9b2fc3df0
Tim Sonnenberg <tim@inpher.io>
2025-04-04 16:51:44 +0200

    feat: add version to constants

--- syntax/internal/stdlib/constants.go
+++ syntax/internal/stdlib/constants.go
@@ -3,12 +3,17 @@ package stdlib
 import (
 	"os"
 	"runtime"
+	"strings"
+	"github.com/blang/semver/v4"
 )
+
+var Version string
 
 var constants = map[string]string{
 	"hostname": "", // Initialized via init function
 	"os":       runtime.GOOS,
 	"arch":     runtime.GOARCH,
+	"version":  Version,
 }
 
 func init() {
@@ -16,4 +21,22 @@ func init() {
 	if err == nil {
 		constants["hostname"] = hostname
 	}
+	constants["version"] = normalizeVersion(Version)
+}
+
+// normalizeVersion normalizes the version string to always contain a "v"
+// prefix. If version cannot be parsed as a semantic version, version is returned unmodified.
+//
+// if version is empty, normalizeVersion returns "v0.0.0".
+func normalizeVersion(version string) string {
+	version = strings.TrimSpace(version)
+	if version == "" {
+		return  "v0.0.0"
+	}
+
+	parsed, err := semver.ParseTolerant(version)
+	if err != nil {
+		return version
+	}
+	return "v" + parsed.String()
 }
