1add7fae583468b686ae239d00a80b297655cdab
Matt Nolf <matt.nolf@grafana.com>
2025-04-16 14:02:07 +0100

    Remove redundant collector name in startup message

--- internal/component/database_observability/mysql/collector/query_sample.go
+++ internal/component/database_observability/mysql/collector/query_sample.go
@@ -98,9 +98,9 @@ func (c *QuerySample) Name() string {
 
 func (c *QuerySample) Start(ctx context.Context) error {
 	if c.disableQueryRedaction {
-		level.Warn(c.logger).Log("msg", QuerySampleName+" collector started with query redaction disabled. Query samples will include complete SQL text including query parameters.")
+		level.Warn(c.logger).Log("msg", "collector started with query redaction disabled. Query samples will include complete SQL text including query parameters.")
 	} else {
-		level.Debug(c.logger).Log("msg", QuerySampleName+" collector started")
+		level.Debug(c.logger).Log("msg", "collector started")
 	}
 
 	c.running.Store(true)
