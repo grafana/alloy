d8b980ec68190d4d61e1689a8e2a05bb87100780
github-hamza-bouqal <hamza.bouqal@teads.tv>
2025-05-16 11:12:44 +0200

    Pyroscope: Bump async-profiler binaries for pyroscope.java

--- internal/component/pyroscope/java/asprof/asprof_linux_arm64.go
+++ internal/component/pyroscope/java/asprof/asprof_linux_arm64.go
@@ -6,7 +6,7 @@ import (
 	_ "embed"
 )
 
-//go:embed async-profiler-3.0-fa937db-linux-arm64.tar.gz
+//go:embed async-profiler-4.0-7c3aa59-linux-arm64.tar.gz
 var embeddedArchiveData []byte
 
 // bin/asprof
