8094333d748034a8ce6f7cb7f0dd0ddd3781e402
Sam DeHaan <sam.dehaan@grafana.com>
2025-06-06 09:38:27 -0400

    Add livedebugging service to validator services list (#3785)
    * Add livedebugging service to validator list
    
    * Update changelog

--- internal/alloycli/cmd_validate.go
+++ internal/alloycli/cmd_validate.go
@@ -12,6 +12,7 @@ import (
 	"github.com/grafana/alloy/internal/service/cluster"
 	"github.com/grafana/alloy/internal/service/http"
 	"github.com/grafana/alloy/internal/service/labelstore"
+	"github.com/grafana/alloy/internal/service/livedebugging"
 	"github.com/grafana/alloy/internal/service/otel"
 	"github.com/grafana/alloy/internal/service/remotecfg"
 	"github.com/grafana/alloy/internal/service/ui"
@@ -70,6 +71,7 @@ func (v *alloyValidate) Run(configFile string) error {
 				&cluster.Service{},
 				&http.Service{},
 				&labelstore.Service{},
+				&livedebugging.Service{},
 				&otel.Service{},
 				&remotecfg.Service{},
 				&ui.Service{},
