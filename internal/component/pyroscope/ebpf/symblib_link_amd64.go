5a699d6092969454a7cb7c398a2e5dae96ac1caf
Tolya Korniltsev <korniltsev.anatoly@gmail.com>
2025-06-09 17:04:47 +0700

    simplify

--- internal/component/pyroscope/ebpf/symblib_link_amd64.go
+++
@@ -1,8 +0,0 @@
-//go:build linux && amd64 && pyroscope_ebpf && !pyroscope_ebpf_no_link
-
-package ebpf
-
-/*
-#cgo LDFLAGS: ${SRCDIR}/../../../../target/x86_64-unknown-linux-musl/release/libsymblib_capi.a
-*/
-import "C"
--- internal/component/pyroscope/ebpf/symblib_link_arm64.go
+++
@@ -1,8 +0,0 @@
-//go:build linux && arm64 && pyroscope_ebpf && !pyroscope_ebpf_no_link
-
-package ebpf
-
-/*
-#cgo LDFLAGS: ${SRCDIR}/../../../../target/aarch64-unknown-linux-musl/release/libsymblib_capi.a
-*/
-import "C"
