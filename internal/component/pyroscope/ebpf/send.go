bd763f8049d16d07a077f500c9da8dff85cce1d6
Tolya Korniltsev <korniltsev.anatoly@gmail.com>
2025-07-14 10:49:23 +0700

    pyroscope.ebpf: otel profiler implementation (#2920)

--- internal/component/pyroscope/ebpf/send.go
+++ internal/component/pyroscope/ebpf/send.go
@@ -1,24 +1,24 @@
-//go:build (linux && arm64) || (linux && amd64)
+//go:build linux && (arm64 || amd64) && pyroscope_ebpf
 
 package ebpf
 
 import (
-	"bytes"
 	"context"
-	"fmt"
 	"time"
 
 	"github.com/go-kit/log/level"
 	"github.com/grafana/alloy/internal/component/pyroscope"
-	"github.com/grafana/pyroscope/ebpf/pprof"
+	"github.com/grafana/alloy/internal/component/pyroscope/ebpf/reporter"
 )
 
 const maxSendConcurrency = 32
 
-func (c *Component) sendProfiles(ctx context.Context, builders *pprof.ProfileBuilders) {
+func (c *Component) sendProfiles(ctx context.Context, ps []reporter.PPROF) {
+	var err error
 	start := time.Now()
 	pool := workerPool{}
-	pool.run(min(maxSendConcurrency, len(builders.Builders)))
+	n := len(ps)
+	pool.run(min(maxSendConcurrency, n))
 	queued := 0
 	ctx, cancel := context.WithTimeout(ctx, c.args.CollectInterval)
 	defer func() {
@@ -27,25 +27,19 @@ func (c *Component) sendProfiles(ctx context.Context, builders *pprof.ProfileBui
 		level.Debug(c.options.Logger).Log("msg", "sent profiles", "duration", time.Since(start), "queued", queued)
 	}()
 	j := 0
-	for _, builder := range builders.Builders {
-		serviceName := builder.Labels.Get("service_name")
+	for _, p := range ps {
+		serviceName := p.Labels.Get("service_name")
 		c.metrics.pprofsTotal.WithLabelValues(serviceName).Inc()
-		c.metrics.pprofSamplesTotal.WithLabelValues(serviceName).Add(float64(len(builder.Profile.Sample)))
+		c.metrics.pprofSamplesTotal.WithLabelValues(serviceName).Add(float64(len(p.Raw)))
 
-		buf := bytes.NewBuffer(nil)
-		_, err := builder.Write(buf)
-		if err != nil {
-			level.Error(c.options.Logger).Log("err", fmt.Errorf("ebpf profile encode %w", err))
-			continue
-		}
-		rawProfile := buf.Bytes()
+		rawProfile := p.Raw
 
 		appender := c.appendable.Appender()
 		c.metrics.pprofBytesTotal.WithLabelValues(serviceName).Add(float64(len(rawProfile)))
 
 		job := func() {
 			samples := []*pyroscope.RawSample{{RawProfile: rawProfile}}
-			err = appender.Append(ctx, builder.Labels, samples)
+			err = appender.Append(ctx, p.Labels, samples)
 			if err != nil {
 				level.Error(c.options.Logger).Log("msg", "ebpf pprof write", "err", err)
 			}
@@ -54,7 +48,7 @@ func (c *Component) sendProfiles(ctx context.Context, builders *pprof.ProfileBui
 		case pool.jobs <- job:
 			queued++
 		case <-ctx.Done():
-			dropped := len(builders.Builders) - j
+			dropped := n - j
 			c.metrics.pprofsDroppedTotal.Add(float64(dropped))
 			level.Debug(c.options.Logger).Log("msg", "dropped profiles", "count", dropped)
 			return
