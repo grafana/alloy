5da139b3e0496ac5aacf4d2c30fe9cd301502d14
Piotr <17101802+thampiotr@users.noreply.github.com>
2025-07-14 14:53:29 +0100

    Reduce allocation in Loki middleware (#3984)
    * Reduce allocation in Loki middleware
    
    * changelog
    
    * clean up
    
    * specify the allocation size when creating log entry
    
    * deal with nil labels
    
    * format

--- internal/component/loki/source/file/decompresser_test.go
+++ internal/component/loki/source/file/decompresser_test.go
@@ -88,10 +88,12 @@ func BenchmarkReadlines(b *testing.B) {
 	for _, tc := range scenarios {
 		b.Run(tc.name, func(b *testing.B) {
 			decBase := &decompressor{
-				logger:   log.NewNopLogger(),
-				running:  atomic.NewBool(false),
-				receiver: loki.NewLogsReceiver(),
-				path:     tc.file,
+				logger:    log.NewNopLogger(),
+				running:   atomic.NewBool(false),
+				receiver:  loki.NewLogsReceiver(),
+				path:      tc.file,
+				positions: &noopPositions{},
+				labels:    model.LabelSet{"foo": "bar", "baz": "boo"},
 			}
 
 			for i := 0; i < b.N; i++ {
