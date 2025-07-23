7353161592f796e399c8762a8f05af36aac785a9
Erik Baranowski <erik.r.baranowski@gmail.com>
2025-07-22 23:41:37 -0400

    make remotecfg metrics registration safe for dups
    Signed-off-by: Erik Baranowski <erik.r.baranowski@gmail.com>

--- internal/service/remotecfg/metrics.go
+++ internal/service/remotecfg/metrics.go
@@ -1,8 +1,9 @@
 package remotecfg
 
 import (
+	"errors"
+
 	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promauto"
 )
 
 type metrics struct {
@@ -16,50 +17,72 @@ type metrics struct {
 }
 
 func registerMetrics(reg prometheus.Registerer) *metrics {
-	prom := promauto.With(reg)
-	return &metrics{
-		configHash: prom.NewGaugeVec(
+	m := &metrics{
+		configHash: prometheus.NewGaugeVec(
 			prometheus.GaugeOpts{
 				Name: "remotecfg_hash",
 				Help: "Hash of the currently active remote configuration.",
 			},
 			[]string{"hash"},
 		),
-		lastLoadSuccess: prom.NewGauge(
+		lastLoadSuccess: prometheus.NewGauge(
 			prometheus.GaugeOpts{
 				Name: "remotecfg_last_load_successful",
 				Help: "Remote config loaded successfully",
 			},
 		),
-		lastFetchNotModified: prom.NewGauge(
+		lastFetchNotModified: prometheus.NewGauge(
 			prometheus.GaugeOpts{
 				Name: "remotecfg_last_load_not_modified",
 				Help: "Remote config not modified since last fetch",
 			},
 		),
-		totalFailures: prom.NewCounter(
+		totalFailures: prometheus.NewCounter(
 			prometheus.CounterOpts{
 				Name: "remotecfg_load_failures_total",
 				Help: "Remote configuration load failures",
 			},
 		),
-		totalAttempts: prom.NewCounter(
+		totalAttempts: prometheus.NewCounter(
 			prometheus.CounterOpts{
 				Name: "remotecfg_load_attempts_total",
 				Help: "Attempts to load remote configuration",
 			},
 		),
-		lastFetchSuccessTime: prom.NewGauge(
+		lastFetchSuccessTime: prometheus.NewGauge(
 			prometheus.GaugeOpts{
 				Name: "remotecfg_last_load_success_timestamp_seconds",
 				Help: "Timestamp of the last successful remote configuration load",
 			},
 		),
-		getConfigTime: prom.NewHistogram(
+		getConfigTime: prometheus.NewHistogram(
 			prometheus.HistogramOpts{
 				Name: "remotecfg_request_duration_seconds",
 				Help: "Duration of remote configuration requests.",
 			},
 		),
+	}
+
+	// Register metrics safely - ignore AlreadyRegisteredError
+	safeRegister(reg, m.configHash)
+	safeRegister(reg, m.lastLoadSuccess)
+	safeRegister(reg, m.lastFetchNotModified)
+	safeRegister(reg, m.totalFailures)
+	safeRegister(reg, m.totalAttempts)
+	safeRegister(reg, m.lastFetchSuccessTime)
+	safeRegister(reg, m.getConfigTime)
+
+	return m
+}
+
+// safeRegister registers a metric with the registerer, ignoring AlreadyRegisteredError
+func safeRegister(reg prometheus.Registerer, c prometheus.Collector) {
+	err := reg.Register(c)
+	if err != nil {
+		var alreadyRegErr prometheus.AlreadyRegisteredError
+		if !errors.As(err, &alreadyRegErr) {
+			panic(err)
+		}
+		// If it is an AlreadyRegisteredError, we ignore it silently
 	}
 }
