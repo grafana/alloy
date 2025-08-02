f2347b752e41c9ef7085d84f1da019f41495dc5b
iamrajiv <rajivperfect007@gmail.com>
2025-08-02 12:31:09 +0530

    add test

---
+++ internal/component/prometheus/echo/echo_test.go
@@ -0,0 +1,143 @@
+package echo
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/go-kit/log"
+	"github.com/grafana/alloy/internal/component"
+	"github.com/grafana/alloy/internal/runtime/componenttest"
+	"github.com/prometheus/prometheus/model/labels"
+	"github.com/prometheus/prometheus/storage"
+	"github.com/stretchr/testify/require"
+)
+
+func TestArgumentsDefaults(t *testing.T) {
+	args := Arguments{}
+	args.SetToDefault()
+	require.Equal(t, DefaultArguments, args)
+}
+
+func TestComponent_Creation(t *testing.T) {
+	ctx := componenttest.TestContext(t)
+
+	comp, err := New(component.Options{
+		ID:     "test",
+		Logger: log.NewNopLogger(),
+	}, Arguments{})
+	require.NoError(t, err)
+	require.NotNil(t, comp)
+
+	ctx, cancel := context.WithTimeout(ctx, time.Second)
+	defer cancel()
+
+	go func() {
+		err := comp.Run(ctx)
+		require.NoError(t, err)
+	}()
+
+	time.Sleep(100 * time.Millisecond)
+}
+
+func TestComponent_ExportsReceiver(t *testing.T) {
+	var exports component.Exports
+
+	comp, err := New(component.Options{
+		ID:     "test",
+		Logger: log.NewNopLogger(),
+		OnStateChange: func(e component.Exports) {
+			exports = e
+		},
+	}, Arguments{})
+	require.NoError(t, err)
+	require.NotNil(t, comp)
+
+	echoExports, ok := exports.(Exports)
+	require.True(t, ok)
+	require.NotNil(t, echoExports.Receiver)
+
+	require.Equal(t, comp, echoExports.Receiver)
+}
+
+func TestAppender_BasicMetrics(t *testing.T) {
+	comp, err := New(component.Options{
+		ID:     "test",
+		Logger: log.NewNopLogger(),
+	}, Arguments{})
+	require.NoError(t, err)
+
+	ctx := context.Background()
+	appender := comp.Appender(ctx)
+	require.NotNil(t, appender)
+
+	lbls := labels.FromStrings("__name__", "test_metric", "job", "test")
+
+	ref, err := appender.Append(0, lbls, time.Now().Unix(), 42.0)
+	require.NoError(t, err)
+	require.NotEqual(t, storage.SeriesRef(0), ref)
+
+	err = appender.Commit()
+	require.NoError(t, err)
+}
+
+func TestAppender_Rollback(t *testing.T) {
+	comp, err := New(component.Options{
+		ID:     "test",
+		Logger: log.NewNopLogger(),
+	}, Arguments{})
+	require.NoError(t, err)
+
+	ctx := context.Background()
+	appender := comp.Appender(ctx)
+	require.NotNil(t, appender)
+
+	lbls := labels.FromStrings("__name__", "test_metric", "job", "test")
+
+	_, err = appender.Append(0, lbls, time.Now().Unix(), 42.0)
+	require.NoError(t, err)
+
+	err = appender.Rollback()
+	require.NoError(t, err)
+}
+
+func TestAppender_MultipleMetrics(t *testing.T) {
+	comp, err := New(component.Options{
+		ID:     "test",
+		Logger: log.NewNopLogger(),
+	}, Arguments{})
+	require.NoError(t, err)
+
+	ctx := context.Background()
+	appender := comp.Appender(ctx)
+	require.NotNil(t, appender)
+
+	metrics := []struct {
+		name  string
+		value float64
+	}{
+		{"metric_one", 1.0},
+		{"metric_two", 2.0},
+		{"metric_three", 3.0},
+	}
+
+	for _, metric := range metrics {
+		lbls := labels.FromStrings("__name__", metric.name, "job", "test")
+		_, err = appender.Append(0, lbls, time.Now().Unix(), metric.value)
+		require.NoError(t, err)
+	}
+
+	err = appender.Commit()
+	require.NoError(t, err)
+}
+
+func TestComponent_Update(t *testing.T) {
+	comp, err := New(component.Options{
+		ID:     "test",
+		Logger: log.NewNopLogger(),
+	}, Arguments{})
+	require.NoError(t, err)
+
+	err = comp.Update(Arguments{})
+	require.NoError(t, err)
+}
