885a5a58003132e8ae252dcf6e4a2eb8d503fc5c
Sam DeHaan <sam.dehaan@grafana.com>
2025-08-06 16:21:53 -0400

    Remove unused remotecfg code unintentionally merged

--- internal/service/remotecfg/remotecfg.go
+++ internal/service/remotecfg/remotecfg.go
@@ -575,29 +575,6 @@ func (s *Service) setPollFrequency(t time.Duration) {
 	}
 }
 
-type agentInterceptor struct {
-	agent string
-}
-
-func (i *agentInterceptor) WrapUnary(next connect.UnaryFunc) connect.UnaryFunc {
-	return func(ctx context.Context, req connect.AnyRequest) (connect.AnyResponse, error) {
-		req.Header().Set("User-Agent", i.agent)
-		return next(ctx, req)
-	}
-}
-
-func (i *agentInterceptor) WrapStreamingClient(next connect.StreamingClientFunc) connect.StreamingClientFunc {
-	return func(ctx context.Context, spec connect.Spec) connect.StreamingClientConn {
-		conn := next(ctx, spec)
-		conn.RequestHeader().Set("User-Agent", i.agent)
-		return conn
-	}
-}
-
-func (i *agentInterceptor) WrapStreamingHandler(next connect.StreamingHandlerFunc) connect.StreamingHandlerFunc {
-	return next
-}
-
 func GetHost(host service.Host) (service.Host, error) {
 	svc, found := host.GetService(ServiceName)
 	if !found {
