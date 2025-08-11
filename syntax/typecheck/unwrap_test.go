d30f588fb2373155598d4c9fc72eca791b43abac
Kalle <23356117+kalleep@users.noreply.github.com>
2025-08-11 13:18:02 +0200

    Add tests for TryUnwrapBlockAttr

--- syntax/typecheck/unwrap_test.go
+++ syntax/typecheck/unwrap_test.go
@@ -1,6 +1,7 @@
 package typecheck
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -62,6 +63,65 @@ func TestUnwrapBlockAttr(t *testing.T) {
 
 			require.Equal(t, v.Type(), tt.expected.Type())
 			require.Equal(t, v.Reflect(), tt.expected.Reflect())
+		})
+	}
+}
+
+func TestTryUnwrapBlockAttr(t *testing.T) {
+	type TestCase struct {
+		desc       string
+		attr       string
+		kind       reflect.Kind
+		src        []byte
+		expected   syntax.Value
+		expectedOk bool
+	}
+
+	tests := []TestCase{
+		{
+			desc:       "unwrap value",
+			attr:       "optional",
+			kind:       reflect.Bool,
+			expectedOk: true,
+			expected:   value.Bool(true),
+			src: []byte(`
+				argument "test" {
+					optional = true
+				}
+			`),
+		},
+		{
+			desc:       "false for wrong type",
+			attr:       "optional",
+			kind:       reflect.Bool,
+			expectedOk: false,
+			expected:   value.Null,
+			src: []byte(`
+				argument "test" {
+					optional = "test"
+				}
+			`),
+		},
+		{
+			desc:       "false for default for missing",
+			attr:       "optional",
+			kind:       reflect.Bool,
+			expectedOk: false,
+			expected:   value.Null,
+			src: []byte(`
+				argument "test" {}
+			`),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			file, err := parser.ParseFile("", tt.src)
+			require.NoError(t, err)
+			v, ok := TryUnwrapBlockAttr(file.Body[0].(*ast.BlockStmt), tt.attr, tt.kind)
+
+			require.Equal(t, tt.expected.Type(), v.Type())
+			require.Equal(t, tt.expectedOk, ok)
 		})
 	}
 }
