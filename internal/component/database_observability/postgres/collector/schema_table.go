1196bfe40d198f0dffb5e9d79c90d00097f7edb7
Shirley Leu <4163034+fridgepoet@users.noreply.github.com>
2025-08-19 20:33:06 -0400

    improve table filters

--- internal/component/database_observability/postgres/collector/schema_table.go
+++ internal/component/database_observability/postgres/collector/schema_table.go
@@ -44,7 +44,8 @@ const (
 	FROM pg_catalog.pg_class pg_class
 	JOIN pg_catalog.pg_namespace pg_namespace ON pg_class.relnamespace = pg_namespace.oid
 	WHERE pg_namespace.nspname = $1 
-		AND pg_class.relkind IN ('r', 'v', 'm', 'f', 'p')`
+		AND pg_class.relkind IN ('r', 'v', 'm', 'f', 'p')  -- filter for application-facing objects
+		AND pg_class.relname NOT LIKE 'pg_%'`
 
 	selectColumnNames = `
 	SELECT
