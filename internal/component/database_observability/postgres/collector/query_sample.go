864a65e8b08035f96499f8968247796a6f1340c4
Cristian Greco <cristian@regolo.cc>
2025-08-26 09:00:39 +0200

    database_observability: don't use slog in postgres collectors (#4257)
    Use Alloy logger instead.

--- internal/component/database_observability/postgres/collector/query_sample.go
+++ internal/component/database_observability/postgres/collector/query_sample.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
-	"log/slog"
 	"time"
 
 	"github.com/go-kit/log"
@@ -24,40 +23,40 @@ const (
 )
 
 const selectPgStatActivity = `
-	SELECT 
+	SELECT
 		clock_timestamp() as now,
-		d.datname,		
+		d.datname,
 		s.pid,
 		s.leader_pid,
-		s.usename,		
+		s.usename,
 		s.application_name,
 		s.client_addr,
 		s.client_port,
 		s.backend_type,
 		s.backend_start,
 		s.backend_xid,
-		s.backend_xmin,		
+		s.backend_xmin,
 		s.xact_start,
 		s.state,
-		s.state_change,		
+		s.state_change,
 		s.wait_event_type,
 		s.wait_event,
 		pg_blocking_pids(s.pid) as blocked_by_pids,
 		s.query_start,
 		s.query_id,
 		s.query
-	FROM pg_stat_activity s 
+	FROM pg_stat_activity s
 		JOIN pg_database d ON s.datid = d.oid AND NOT d.datistemplate AND d.datallowconn
-	WHERE 
+	WHERE
 		s.pid <> pg_backend_pid() AND
 		(
 			s.backend_type != 'client backend' OR
 			(
-				coalesce(TRIM(s.query), '') != '' AND s.query_start IS NOT NULL AND 
+				coalesce(TRIM(s.query), '') != '' AND s.query_start IS NOT NULL AND
 				(
-					s.state != 'idle' OR 
+					s.state != 'idle' OR
 					(s.state = 'idle' AND s.state_change > $1)
-				) AND 
+				) AND
 				coalesce(TRIM(s.state), '') != ''
 			)
 		)
@@ -177,7 +176,6 @@ func calculateDuration(nullableTime sql.NullTime, currentTime time.Time) string 
 }
 
 func (c *QuerySample) fetchQuerySample(ctx context.Context) error {
-	slog.Debug("Fetching sample")
 	scrapeTime := time.Now()
 	rows, err := c.dbConnection.QueryContext(ctx, selectPgStatActivity, c.lastScrape)
 	if err != nil {
