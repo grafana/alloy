0202fc557493c1e0e039db207e46932f0cc3e214
Sam DeHaan <sam.dehaan@grafana.com>
2025-10-14 23:02:20 +0200

    Update tests and converter

--- internal/component/otelcol/exporter/splunkhec/config/splunkhec_test.go
+++ internal/component/otelcol/exporter/splunkhec/config/splunkhec_test.go
@@ -204,6 +204,30 @@ func TestUnmarshalSplunkHecArguments(t *testing.T) {
 		`,
 			expectErr: false,
 		},
+		{
+			name: "valid splunkhec arguments, with otelattrs",
+			cfg: `
+			splunk {
+				log_data_enabled = true
+				profiling_data_enabled = true
+				source = "def"
+				token = "abc"
+	     	}
+	     	client  {
+			    endpoint = "http://localhost:8088"
+	     		timeout = "10s"
+	     		insecure_skip_verify = true
+	     		max_idle_conns = 10
+			}
+			otel_attrs_to_hec_metadata {
+				source = "mysource"
+				sourcetype = "mysourcetype"
+				index = "myindex"
+				host = "myhost"
+			}
+		`,
+			expectErr: false,
+		},
 	} {
 		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
