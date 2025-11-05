99d1bfd9f7db5bd038da86225220bcb72530ffeb
Kalle <23356117+kalleep@users.noreply.github.com>
2025-11-05 13:48:06 +0100

    squash into config.go and remove unused things

--- internal/loki/promtail/stages/eventlogmessage.go
+++
@@ -1,7 +0,0 @@
-package stages
-
-type EventLogMessageConfig struct {
-	Source            *string `mapstructure:"source"`
-	DropInvalidLabels bool    `mapstructure:"drop_invalid_labels"`
-	OverwriteExisting bool    `mapstructure:"overwrite_existing"`
-}
