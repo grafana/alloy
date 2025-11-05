bd4fcd02e689b9cf161fbf819dcd0c1e30a1c146
Bryan Boreham <bjboreham@gmail.com>
2025-11-05 09:54:27 +0000

    Fix races reported in Alloy tests  (#4059)
    * Fix races reported in Alloy tests
    
    One race remains in the external node_exporter package.
    
    Adjusted make rule and clarified comment about this.
    
    Signed-off-by: Bryan Boreham <bjboreham@gmail.com>
    
    * Run packaging tests with -race
    
    Find out if there are any races.
    
    Signed-off-by: Bryan Boreham <bjboreham@gmail.com>
    
    ---------
    
    Signed-off-by: Bryan Boreham <bjboreham@gmail.com>

--- internal/tools/packaging_test/alloy_linux_packages_test.go
+++ internal/tools/packaging_test/alloy_linux_packages_test.go
@@ -1,5 +1,5 @@
-//go:build !nonetwork && !nodocker && !race && packaging
-// +build !nonetwork,!nodocker,!race,packaging
+//go:build !nonetwork && !nodocker && packaging
+// +build !nonetwork,!nodocker,packaging
 
 package packaging_test
 
