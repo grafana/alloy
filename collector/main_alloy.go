e4e25e47716f8b9c8c39a8011098ddabd6087038
Bejal Lewis <bejal.lewis@grafana.com>
2025-11-15 14:14:38 +0100

    Add basic OTel engine

---
+++ collector/main_alloy.go
@@ -0,0 +1,20 @@
+// GENERATED CODE: DO NOT EDIT (the .go file, editing the .tpl file is okay)
+package main
+
+import (
+	"github.com/grafana/alloy/flowcmd"
+	"github.com/spf13/cobra"
+	"go.opentelemetry.io/collector/otelcol"
+)
+
+func newAlloyCommand(params otelcol.CollectorSettings) *cobra.Command {
+    otelCmd := otelcol.NewCommand(params)
+    // Modify the command to fit better in Alloy
+    otelCmd.Use = "otel"
+    otelCmd.Short = "Alloy OTel Collector runtime mode"
+    otelCmd.Long = "Use Alloy with OpenTelemetry Collector runtime"
+
+    flowCmd := flowcmd.RootCommand()
+    flowCmd.AddCommand(otelCmd)
+    return flowCmd
+}
