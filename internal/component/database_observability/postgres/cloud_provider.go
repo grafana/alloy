76b82b80f2f1fa99efc318450f9d433d882e6041
Matt Nolf <matt.nolf@grafana.com>
2025-11-26 18:53:34 +0000

    Add cloud provider labels to all metrics

---
+++ internal/component/database_observability/postgres/cloud_provider.go
@@ -0,0 +1,61 @@
+package postgres
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+
+	"github.com/aws/aws-sdk-go-v2/aws/arn"
+	"github.com/grafana/alloy/internal/component/database_observability"
+	"github.com/grafana/alloy/internal/component/database_observability/postgres/collector"
+)
+
+var (
+	rdsRegex   = regexp.MustCompile(`(?P<identifier>[^\.]+)\.([^\.]+)\.(?P<region>[^\.]+)\.rds\.amazonaws\.com`)
+	azureRegex = regexp.MustCompile(`(?P<identifier>[^\.]+)\.postgres\.database\.azure\.com`)
+)
+
+func populateCloudProviderFromConfig(config *CloudProvider) (*database_observability.CloudProvider, error) {
+	var cloudProvider database_observability.CloudProvider
+	if config.AWS != nil {
+		arn, err := arn.Parse(config.AWS.ARN)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse AWS cloud provider ARN: %w", err)
+		}
+		cloudProvider.AWS = &database_observability.AWSCloudProviderInfo{
+			ARN: arn,
+		}
+	}
+	return &cloudProvider, nil
+}
+
+func populateCloudProviderFromDSN(dsn string) (*database_observability.CloudProvider, error) {
+	var cloudProvider database_observability.CloudProvider
+
+	parts, err := collector.ParseURL(dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if host, ok := parts["host"]; ok {
+		if strings.HasSuffix(host, "rds.amazonaws.com") {
+			matches := rdsRegex.FindStringSubmatch(host)
+			cloudProvider.AWS = &database_observability.AWSCloudProviderInfo{
+				ARN: arn.ARN{
+					Resource:  fmt.Sprintf("db:%s", matches[1]),
+					Region:    matches[3],
+					AccountID: "unknown",
+				},
+			}
+		} else if strings.HasSuffix(host, "postgres.database.azure.com") {
+			matches := azureRegex.FindStringSubmatch(host)
+			if len(matches) > 1 {
+				cloudProvider.Azure = &database_observability.AzureCloudProviderInfo{
+					Resource: matches[1],
+				}
+			}
+		}
+	}
+
+	return &cloudProvider, nil
+}
