17546a4667f6cf8c9e0d1deacdd1901d156f66c8
Kalle <23356117+kalleep@users.noreply.github.com>
2025-12-04 10:33:00 +0100

    fix

--- internal/component/loki/process/stages/extensions.go
+++ internal/component/loki/process/stages/extensions.go
@@ -190,7 +190,7 @@ func NewCRI(logger log.Logger, config CRIConfig, registerer prometheus.Registere
 		},
 		{
 			OutputConfig: &OutputConfig{
-				"content",
+				Source: "content",
 			},
 		},
 	}
