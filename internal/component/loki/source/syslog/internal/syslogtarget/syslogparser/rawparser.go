8a59b17b5522031f2584dcf8e6777f995149669b
x1unix <9203548+x1unix@users.noreply.github.com>
2025-12-15 20:12:45 -0500

    feat: implement raw parser

---
+++ internal/component/loki/source/syslog/internal/syslogtarget/syslogparser/rawparser.go
@@ -0,0 +1,135 @@
+package syslogparser
+
+import (
+	"bufio"
+	"bytes"
+	"errors"
+	"fmt"
+	"io"
+	"strconv"
+	"unicode"
+
+	"github.com/leodido/go-syslog/v4"
+)
+
+// ReadLineRaw reads a single syslog line without syslog contents parsing.
+//
+// Delimiter argument is used to determine line end for non-transparent framing.
+//
+// Meant to be used in UDP transport where a single syslog line is delivered per datagram.
+func ReadLineRaw(r io.Reader, delimiter byte) (*syslog.Base, error) {
+	buf := bufio.NewReaderSize(r, 1<<10)
+
+	b, err := buf.ReadByte()
+	if err != nil {
+		return nil, err
+	}
+
+	// TODO: use bytebufferpool?
+	_ = buf.UnreadByte()
+	switch framingTypeFromFirstByte(b) {
+	case framingTypeNonTransparent:
+		// TODO: read until newline
+		buff, err := buf.ReadBytes(delimiter)
+		if err != nil {
+			// Ignore io.EOF if some data was returned
+			if !errors.Is(err, io.EOF) || len(buff) == 0 {
+				return nil, err
+			}
+		}
+
+		if len(buff) == 0 {
+			return nil, io.EOF
+		}
+
+		// trim potential newline leftovers if called sequentially inside TCP conn.
+		buff = bytes.TrimFunc(buff, unicode.IsSpace)
+		return readLogLine(buff), nil
+
+	case framingTypeOctetCounting:
+		contentLength, err := readFrameLength(buf)
+		if err != nil {
+			return nil, fmt.Errorf("failed to read octet length header: %w", err)
+		}
+
+		// Skip space after octet length. Octet counting refers to length after a space.
+		_, _ = buf.ReadByte()
+		buff := make([]byte, contentLength)
+		n, err := buf.Read(buff)
+		if err != nil {
+			return nil, fmt.Errorf("cannot read message: %w (length: %d)", err, contentLength)
+		}
+
+		if n == 0 {
+			return nil, fmt.Errorf("empty buffer returned (expected: %d)", contentLength)
+		}
+
+		buff = buff[:n]
+		return readLogLine(buff), nil
+	default:
+		return nil, fmt.Errorf("invalid or unsupported framing. first byte: %q", b)
+	}
+}
+
+func readLogLine(line []byte) *syslog.Base {
+	out := &syslog.Base{}
+	line = readSeverity(line, out)
+
+	msg := string(bytes.TrimSpace(line))
+	out.Message = &msg
+	return out
+}
+
+func readSeverity(line []byte, dst *syslog.Base) (next []byte) {
+	// priority has to be in format '<0-9+>'
+	if len(line) < 3 || line[0] != '<' {
+		return line
+	}
+
+	buff := line[1:]
+	priority := uint(0)
+	for i, v := range buff {
+		if v == '>' {
+			if i == 0 || priority > 255 {
+				return line
+			}
+
+			severity := uint8(priority % 8)
+			dst.Severity = &severity
+			dst.Priority = new(uint8)
+			*dst.Priority = uint8(priority)
+
+			buff = buff[i+1:]
+			return buff
+		}
+
+		if !isDigit(v) {
+			return line
+		}
+
+		priority *= 10
+		priority += uint(v - '0')
+	}
+
+	return line
+}
+
+func readFrameLength(r *bufio.Reader) (flen int, err error) {
+	// log lines with octet counted framing start with length.
+	// Example: `114 <34>1 2025-01-03T14:07:15.003Z message...`
+	part, err := r.ReadString(' ')
+	if err != nil {
+		return 0, fmt.Errorf("%w (read: %q)", err, part)
+	}
+
+	c, err := strconv.ParseInt(part, 10, 64)
+	if err != nil {
+		return 0, fmt.Errorf("failed to parse octet length from %q: %w", part, err)
+	}
+
+	return int(c), nil
+}
+
+func ParseStreamRaw(r io.Reader) error {
+	return nil
+}
--- internal/component/loki/source/syslog/internal/syslogtarget/syslogparser/syslogparser.go
+++ internal/component/loki/source/syslog/internal/syslogtarget/syslogparser/syslogparser.go
@@ -6,12 +6,43 @@ import (
 	"io"
 	"time"
 
-	"github.com/grafana/alloy/internal/util"
 	"github.com/leodido/go-syslog/v4"
 	"github.com/leodido/go-syslog/v4/nontransparent"
 	"github.com/leodido/go-syslog/v4/octetcounting"
 	"github.com/leodido/go-syslog/v4/rfc3164"
+
+	"github.com/grafana/alloy/internal/util"
 )
+
+type framingType = uint
+
+const (
+	framingTypeUnknown framingType = iota
+	framingTypeOctetCounting
+	framingTypeNonTransparent
+)
+
+// framingTypeFromFirstByte detects framing type from a first byte of syslog line.
+// Returns [framingTypeNonTransparent] on failure.
+//
+// See https://datatracker.ietf.org/doc/html/rfc6587 for details on message framing.
+func framingTypeFromFirstByte(b byte) framingType {
+	if b == '<' {
+		// Message starts with log severity and no length, should be consumed until '\n' or '\0' character.
+		return framingTypeNonTransparent
+	}
+
+	if isDigit(b) {
+		// Message starts with content length.
+		return framingTypeOctetCounting
+	}
+
+	return framingTypeUnknown
+}
+
+func isDigit(b byte) bool {
+	return b >= '0' && b <= '9'
+}
 
 // ParseStream parses a rfc5424 syslog stream from the given Reader, calling
 // the callback function with the parsed messages. The parser automatically
@@ -41,21 +72,22 @@ func ParseStream(isRFC3164Message bool, useRFC3164DefaultYear bool, r io.Reader,
 	// See https://datatracker.ietf.org/doc/html/rfc6587 for details on message framing
 	// If a syslog message starts with '<' the first piece of the message is the priority, which means it must use
 	// an explicit framing character.
-	if b == '<' {
+	switch framingTypeFromFirstByte(b) {
+	case framingTypeNonTransparent:
 		if isRFC3164Message {
 			nontransparent.NewParserRFC3164(syslog.WithListener(cb), syslog.WithMaxMessageLength(maxMessageLength), syslog.WithBestEffort()).Parse(buf)
 		} else {
 			nontransparent.NewParser(syslog.WithListener(cb), syslog.WithMaxMessageLength(maxMessageLength), syslog.WithBestEffort()).Parse(buf)
 		}
+	case framingTypeOctetCounting:
 		// If a syslog message starts with a digit, it must use octet counting, and the first piece of the message is the length
-	} else if b >= '0' && b <= '9' {
 		if isRFC3164Message {
 			octetcounting.NewParserRFC3164(syslog.WithListener(cb), syslog.WithMaxMessageLength(maxMessageLength), syslog.WithBestEffort()).Parse(buf)
 		} else {
 			octetcounting.NewParser(syslog.WithListener(cb), syslog.WithMaxMessageLength(maxMessageLength), syslog.WithBestEffort()).Parse(buf)
 		}
-	} else {
-		return fmt.Errorf("invalid or unsupported framing. first byte: '%s'", string(b))
+	default:
+		return fmt.Errorf("invalid or unsupported framing. first byte: %q", b)
 	}
 
 	return nil
