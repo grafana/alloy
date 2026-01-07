f9bad174d480274443239b1780181db318dc36b1
Bejal Lewis <bejal.lewis@grafana.com>
2026-01-07 12:23:51 +0100

    Add back generated files

---
+++ collector/main_alloy.go
@@ -0,0 +1,23 @@
+// GENERATED CODE: DO NOT EDIT
+package main
+
+import (
+	"github.com/grafana/alloy/flowcmd"
+	"github.com/spf13/cobra"
+	"go.opentelemetry.io/collector/otelcol"
+)
+
+func newAlloyCommand(params otelcol.CollectorSettings) *cobra.Command {
+	otelCmd := otelcol.NewCommand(params)
+
+	otelCmd.Use = "otel"
+	otelCmd.Short = "Use Alloy with OTel Engine"
+	otelCmd.Long = "[EXPERIMENTAL] Use Alloy with OpenTelemetry Collector Engine"
+
+	flowCmd := flowcmd.RootCommand()
+	flowCmd.AddCommand(otelCmd)
+
+	// TODO: Officially add this command to expose in a follow up PR
+	otelCmd.Hidden = true
+	return flowCmd
+}
