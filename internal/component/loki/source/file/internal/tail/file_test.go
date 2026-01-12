7bc73f77ecfd9918938b7f2014de9e57ca5ebece
Kalle <23356117+kalleep@users.noreply.github.com>
2026-01-12 10:46:59 +0100

    Add more test

--- internal/component/loki/source/file/internal/tail/file_test.go
+++ internal/component/loki/source/file/internal/tail/file_test.go
@@ -285,6 +285,30 @@ func TestFile(t *testing.T) {
 		verify(t, file, &Line{Text: "line4", Offset: 24}, nil)
 		verify(t, file, &Line{Text: "newline1", Offset: 33}, nil)
 	})
+
+	t.Run("should handle atomic writes with new content", func(t *testing.T) {
+		name := createFile(t, "atomicwrite", "line1\nline2\nline3\nline4\n")
+		defer removeFile(t, name)
+
+		file, err := NewFile(log.NewNopLogger(), &Config{
+			Filename: name,
+			WatcherConfig: WatcherConfig{
+				MinPollFrequency: 50 * time.Millisecond,
+				MaxPollFrequency: 50 * time.Millisecond,
+			},
+		})
+		require.NoError(t, err)
+		defer file.Stop()
+
+		// Read first two lines
+		verify(t, file, &Line{Text: "line1", Offset: 6}, nil)
+		verify(t, file, &Line{Text: "line2", Offset: 12}, nil)
+		atomicwrite(t, name, "newline1\n")
+		// Because we buffer lines when file is deleted we still get line3 and line3.
+		verify(t, file, &Line{Text: "line3", Offset: 18}, nil)
+		verify(t, file, &Line{Text: "line4", Offset: 24}, nil)
+		verify(t, file, &Line{Text: "newline1", Offset: 9}, nil)
+	})
 }
 
 func createFile(t *testing.T, name, content string) string {
