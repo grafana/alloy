b314ed8404826f97f706b6ae65ecf27c67c50ca9
Kalle <23356117+kalleep@users.noreply.github.com>
2026-01-19 16:11:31 +0100

    rename

---
+++ integration-tests/docker/tests/otlp-metrics/otlp_metrics_test.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/grafana/alloy/integration-tests/docker/common"
+)
+
+func TestOTLPMetrics(t *testing.T) {
+	common.MimirMetricsTest(t, common.OtelDefaultMetrics, common.OtelDefaultHistogramMetrics, "otlp_metrics")
+}
