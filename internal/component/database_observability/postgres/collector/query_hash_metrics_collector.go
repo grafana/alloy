a5bb0f49cd4236e8b6dd5d27884543baac17e5a1
Gabriel <g.amaral.antunes@gmail.com>
2026-01-27 10:43:58 -0300

    remove unecessary test

--- internal/component/database_observability/postgres/collector/query_hash_metrics_collector.go
+++ internal/component/database_observability/postgres/collector/query_hash_metrics_collector.go
@@ -1,8 +1,67 @@
 package collector
 
 import (
+	"sync"
+	"time"
+
+	"github.com/hashicorp/golang-lru/v2/expirable"
 	"github.com/prometheus/client_golang/prometheus"
 )
+
+// QueryHashInfo contains information about a query hash
+type QueryHashInfo struct {
+	QueryHash    string
+	DatabaseName string
+	LastSeen     time.Time
+}
+
+// QueryHashRegistry maintains a mapping of queryid to queryhash
+type QueryHashRegistry struct {
+	mu    sync.RWMutex
+	cache *expirable.LRU[string, QueryHashInfo]
+}
+
+// NewQueryHashRegistry creates a new QueryHashRegistry
+func NewQueryHashRegistry(size int, ttl time.Duration) *QueryHashRegistry {
+	return &QueryHashRegistry{
+		cache: expirable.NewLRU[string, QueryHashInfo](size, nil, ttl),
+	}
+}
+
+// Set stores a queryid to queryhash mapping
+func (r *QueryHashRegistry) Set(queryID, queryHash, databaseName string) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	r.cache.Add(queryID, QueryHashInfo{
+		QueryHash:    queryHash,
+		DatabaseName: databaseName,
+		LastSeen:     time.Now(),
+	})
+}
+
+// Get retrieves the QueryHashInfo for a given queryID
+func (r *QueryHashRegistry) Get(queryID string) (QueryHashInfo, bool) {
+	r.mu.RLock()
+	defer r.mu.RUnlock()
+
+	info, ok := r.cache.Get(queryID)
+	return info, ok
+}
+
+// GetAll returns all current queryid to queryhash mappings
+func (r *QueryHashRegistry) GetAll() map[string]QueryHashInfo {
+	r.mu.RLock()
+	defer r.mu.RUnlock()
+
+	result := make(map[string]QueryHashInfo)
+	for _, key := range r.cache.Keys() {
+		if info, ok := r.cache.Peek(key); ok {
+			result[key] = info
+		}
+	}
+	return result
+}
 
 // QueryHashMetricsCollector exposes queryid to queryhash mappings as Prometheus metrics
 type QueryHashMetricsCollector struct {
