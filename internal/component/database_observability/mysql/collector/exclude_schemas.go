1fe42db8f0709757aac46d8f7e86c63802be6416
Cristian Greco <cristian@regolo.cc>
2026-01-28 15:56:54 +0100

    feat: Database_observability.postgres: support excluding DBs in all collectors (#5366)
    ### Brief description of Pull Request
    Propagate `exclude_databases` configuration to all PostgreSQL collectors
    and append the user-defined databases to the default excluded databases.
    
    Additionally, change the behaviour of `explain_plans` collector to skip
    queries from excluded databases instead of processing them.
    
    Followup of https://github.com/grafana/alloy/pull/5350
    
    ### Pull Request Details
    
    <!-- Add a more detailed descripion of the Pull Request here, if needed.
    -->
    
    ### Issue(s) fixed by this Pull Request
    
    <!--
    Uncomment the following line and fill in an issue number if you want a
    GitHub
      issue to be closed automatically when this PR gets merged.
    -->
    
    <!-- Fixes #issue_id -->
    
    ### Notes to the Reviewer
    
    <!-- Add any relevant notes for the reviewers and testers of this PR.
    -->
    
    ### PR Checklist
    
    <!-- Remove items that do not apply. For completed items, change [ ] to
    [x]. -->
    
    - [ ] Documentation added
    - [x] Tests updated
    - [ ] Config converters updated

--- internal/component/database_observability/mysql/collector/exclude_schemas.go
+++ internal/component/database_observability/mysql/collector/exclude_schemas.go
@@ -1,33 +1,18 @@
 package collector
 
-import "strings"
+import "github.com/grafana/alloy/internal/component/database_observability"
 
-var defaultExcludedSchemas = []string{"mysql", "performance_schema", "sys", "information_schema"}
+var excludedSchemas = []string{"mysql", "performance_schema", "sys", "information_schema"}
 
-var defaultExclusionClause = buildExclusionClause(defaultExcludedSchemas)
+var exclusionClause = database_observability.BuildExclusionClause(excludedSchemas)
 
-func buildExcludedSchemasClause(excludedSchemas []string) string {
-	if len(excludedSchemas) == 0 {
-		return defaultExclusionClause
+func buildExcludedSchemasClause(schemas []string) string {
+	if len(schemas) == 0 {
+		return exclusionClause
 	}
 
-	allSchemas := make([]string, 0, len(defaultExcludedSchemas)+len(excludedSchemas))
-	allSchemas = append(allSchemas, defaultExcludedSchemas...)
-	allSchemas = append(allSchemas, excludedSchemas...)
-
-	return buildExclusionClause(allSchemas)
-}
-
-func buildExclusionClause(schemas []string) string {
-	escaped := make([]string, len(schemas))
-	for i, schema := range schemas {
-		escaped[i] = escapeSQLString(schema)
-	}
-	return "(" + strings.Join(escaped, ", ") + ")"
-}
-
-// escapeSQLString escapes single quotes by doubling them to prevent SQL injection.
-func escapeSQLString(s string) string {
-	escaped := strings.ReplaceAll(s, "'", "''")
-	return "'" + escaped + "'"
+	all := make([]string, 0, len(excludedSchemas)+len(schemas))
+	all = append(all, excludedSchemas...)
+	all = append(all, schemas...)
+	return database_observability.BuildExclusionClause(all)
 }
