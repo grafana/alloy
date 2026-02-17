bde12253d90f4502256581b3ab73d1256576fe86
x1unix <9203548+x1unix@users.noreply.github.com>
2026-02-17 17:41:38 -0500

    feat: remove obsolete BenchmarkStoreFlows and relocate BenchmarkAppenderFlows

--- internal/component/prometheus/fanout_test.go
+++ internal/component/prometheus/fanout_test.go
@@ -1,29 +1,87 @@
-package prometheus
+package prometheus_test
 
 import (
+	"fmt"
 	"testing"
+	"time"
 
-	"github.com/prometheus/client_golang/prometheus"
-
+	"github.com/go-kit/log"
+	promclient "github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/prometheus/storage"
-
-	"github.com/grafana/alloy/internal/service/labelstore"
-
 	"github.com/stretchr/testify/require"
+	"go.uber.org/atomic"
+
+	"github.com/grafana/alloy/internal/component/prometheus"
+	"github.com/grafana/alloy/internal/component/prometheus/remotewrite"
+	"github.com/grafana/alloy/internal/service/labelstore"
 )
 
 func TestRollback(t *testing.T) {
-	ls := labelstore.New(nil, prometheus.DefaultRegisterer)
-	fanout := NewFanout([]storage.Appendable{NewFanout(nil, "1", prometheus.DefaultRegisterer, ls)}, "", prometheus.DefaultRegisterer, ls)
+	ls := labelstore.New(nil, promclient.DefaultRegisterer)
+	fanout := prometheus.NewFanout([]storage.Appendable{prometheus.NewFanout(nil, "1", promclient.DefaultRegisterer, ls)}, "", promclient.DefaultRegisterer, ls)
 	app := fanout.Appender(t.Context())
 	err := app.Rollback()
 	require.NoError(t, err)
 }
 
 func TestCommit(t *testing.T) {
-	ls := labelstore.New(nil, prometheus.DefaultRegisterer)
-	fanout := NewFanout([]storage.Appendable{NewFanout(nil, "1", prometheus.DefaultRegisterer, ls)}, "", prometheus.DefaultRegisterer, ls)
+	ls := labelstore.New(nil, promclient.DefaultRegisterer)
+	fanout := prometheus.NewFanout([]storage.Appendable{prometheus.NewFanout(nil, "1", promclient.DefaultRegisterer, ls)}, "", promclient.DefaultRegisterer, ls)
 	app := fanout.Appender(t.Context())
 	err := app.Commit()
 	require.NoError(t, err)
+}
+
+func BenchmarkAppenderFlows(b *testing.B) {
+	numberOfMetrics := []int{2000}
+	for _, n := range numberOfMetrics {
+		metrics := setupMetrics(n)
+		now := time.Now().UnixMilli()
+
+		testName := fmt.Sprintf("labelstore/new-metrics=%d", n)
+		ls := labelstore.New(log.NewNopLogger(), promclient.DefaultRegisterer)
+		rw1 := remotewrite.NewInterceptor("1", &atomic.Bool{}, noopDebugDataPublisher{}, ls, noopStore{})
+		rw2 := remotewrite.NewInterceptor("2", &atomic.Bool{}, noopDebugDataPublisher{}, ls, noopStore{})
+		children := []storage.Appendable{rw1, rw2}
+		fanout := prometheus.NewFanout(children, "fanout", promclient.DefaultRegisterer, ls)
+
+		b.Run(testName, func(b *testing.B) {
+			b.ReportAllocs()
+			b.ResetTimer()
+			for b.Loop() {
+				app := fanout.Appender(b.Context())
+				for _, metric := range metrics {
+					app.Append(0, metric, now, 1.0)
+				}
+				app.Commit()
+
+				b.StopTimer()
+				fanout.Clear()
+				b.StartTimer()
+			}
+		})
+
+		testName = fmt.Sprintf("seriesref/new-metrics=%d", n)
+		ls = labelstore.New(log.NewNopLogger(), promclient.DefaultRegisterer, false)
+		rw1 = remotewrite.NewInterceptor("1", &atomic.Bool{}, noopDebugDataPublisher{}, ls, noopStore{})
+		rw2 = remotewrite.NewInterceptor("2", &atomic.Bool{}, noopDebugDataPublisher{}, ls, noopStore{})
+		children = []storage.Appendable{rw1, rw2}
+		fanout = prometheus.NewFanout(children, "fanout", promclient.DefaultRegisterer, ls)
+
+		b.Run(testName, func(b *testing.B) {
+			b.ReportAllocs()
+			b.ResetTimer()
+			for b.Loop() {
+				app := fanout.Appender(b.Context())
+				for _, metric := range metrics {
+					app.Append(0, metric, now, 1.0)
+				}
+				app.Commit()
+
+				b.StopTimer()
+				fanout.Clear()
+				b.StartTimer()
+			}
+		})
+	}
 }
