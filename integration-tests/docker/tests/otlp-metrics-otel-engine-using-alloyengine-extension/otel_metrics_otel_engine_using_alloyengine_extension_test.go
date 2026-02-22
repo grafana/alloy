ddc9004629ed9787800f419b0f9e542ffc2fc3cd
Joe Harvey <51208233+jharvey10@users.noreply.github.com>
2026-02-22 02:30:06 -0500

    test: compose container name cleanup

--- integration-tests/docker/tests/otlp-metrics-otel-engine-using-alloyengine-extension/otel_metrics_otel_engine_using_alloyengine_extension_test.go
+++ integration-tests/docker/tests/otlp-metrics-otel-engine-using-alloyengine-extension/otel_metrics_otel_engine_using_alloyengine_extension_test.go
@@ -8,6 +8,11 @@ import (
 	"github.com/grafana/alloy/integration-tests/docker/common"
 )
 
-func TestOtelMetricsAlloyengine(t *testing.T) {
-	common.MimirMetricsTest(t, common.OtelDefaultMetrics, common.OtelDefaultHistogramMetrics, "otel_metrics_alloyengine")
+func TestOtelMetricsOtelEngineUsingAlloyengineExtension(t *testing.T) {
+	common.MimirMetricsTest(
+		t,
+		common.OtelDefaultMetrics,
+		common.OtelDefaultHistogramMetrics,
+		"otel_metrics_otel_engine_using_alloyengine_extension",
+	)
 }
