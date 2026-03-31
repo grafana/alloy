1e3334bca541fe3e77521f3203c95a0b4b4a26ff
Cristian Greco <cristian@regolo.cc>
2026-03-31 09:35:24 +0200

    fix(database_observability): Ensure connection_info_monitor goroutine exits on Stop (#5874)
    ### Brief description of Pull Request
    Fix race condition where `Stop()` could return before the monitor
    goroutine had exited.
    
    Followup of https://github.com/grafana/alloy/pull/5796
    
    ### Pull Request Details
    
    <!-- Add a more detailed descripion of the Pull Request here, if needed.
    -->
    
    ### Issue(s) fixed by this Pull Request
    
    <!--
    Uncomment the following line and fill in an issue number if you want a
    GitHub
      issue to be closed automatically when this PR gets merged.
    -->
    
    <!-- Fixes #issue_id -->
    
    ### Notes to the Reviewer
    
    <!-- Add any relevant notes for the reviewers and testers of this PR.
    -->
    
    ### PR Checklist
    
    <!-- Remove items that do not apply. For completed items, change [ ] to
    [x]. -->
    
    - [ ] Documentation added
    - [ ] Tests updated
    - [ ] Config converters updated

--- internal/component/database_observability/postgres/collector/connection_info.go
+++ internal/component/database_observability/postgres/collector/connection_info.go
@@ -32,7 +32,7 @@ type ConnectionInfo struct {
 	dbConnection  *sql.DB
 
 	running *atomic.Bool
-	cancel  context.CancelFunc
+	stop    func()
 }
 
 func NewConnectionInfo(args ConnectionInfoArguments) (*ConnectionInfo, error) {
@@ -120,7 +120,7 @@ func (c *ConnectionInfo) Start(ctx context.Context) error {
 	c.InfoMetric.WithLabelValues(labelValues...).Set(1)
 
 	if c.dbConnection != nil {
-		c.cancel = database_observability.RunConnectionInfoMonitor(
+		c.stop = database_observability.RunConnectionInfoMonitor(
 			ctx,
 			c.dbConnection,
 			c.Registry,
@@ -139,8 +139,8 @@ func (c *ConnectionInfo) Stopped() bool {
 }
 
 func (c *ConnectionInfo) Stop() {
-	if c.cancel != nil {
-		c.cancel()
+	if c.stop != nil {
+		c.stop()
 	}
 	c.Registry.Unregister(c.InfoMetric)
 	c.running.Store(false)
