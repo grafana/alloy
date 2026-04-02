55ca75fde7d698681c2f3cbc35905a85a5aa767f
Kalle <23356117+kalleep@users.noreply.github.com>
2026-04-02 14:37:53 +0200

    chore: Add intetgration test for loki.source.kafka

---
+++ integration-tests/docker/tests/loki-kafka/loki_kafka_test.go
@@ -0,0 +1,106 @@
+//go:build alloyintegrationtests
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"strconv"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/IBM/sarama"
+	"github.com/stretchr/testify/require"
+
+	"github.com/grafana/alloy/integration-tests/docker/common"
+	"github.com/grafana/dskit/backoff"
+)
+
+var topics = []string{"test1", "test2", "test3"}
+
+const (
+	numMessages = 5
+	broker      = "localhost:29092"
+)
+
+func TestLokiKafka(t *testing.T) {
+	producer, err := newProducer()
+	require.NoError(t, err)
+	defer func() { require.NoError(t, producer.Close()) }()
+
+	var (
+		wg   sync.WaitGroup
+		errs = make([]error, len(topics))
+	)
+
+	for i, t := range topics {
+		wg.Go(func() { errs[i] = sendMessages(producer, t, numMessages) })
+	}
+
+	wg.Wait()
+	for _, err := range errs {
+		require.NoError(t, err)
+	}
+
+	var (
+		total      = numMessages * len(topics)
+		assertions = make([]common.ExpectedLogResult, 0, len(topics))
+	)
+
+	for _, t := range topics {
+		assertions = append(assertions, common.ExpectedLogResult{
+			Labels: map[string]string{
+				"topic": t,
+			},
+			EntryCount: numMessages,
+		})
+	}
+
+	common.AssertLogsPresent(
+		t,
+		total,
+		assertions...,
+	)
+}
+
+func newProducer() (sarama.SyncProducer, error) {
+	config := sarama.NewConfig()
+	config.Producer.Return.Successes = true
+
+	backoff := backoff.New(context.Background(), backoff.Config{
+		MinBackoff: 50 * time.Millisecond,
+		MaxBackoff: 5 * time.Second,
+		MaxRetries: 10,
+	})
+
+	var (
+		lastErr  error
+		producer sarama.SyncProducer
+	)
+
+	for backoff.Ongoing() {
+		producer, lastErr = sarama.NewSyncProducer([]string{broker}, config)
+		if lastErr == nil {
+			return producer, nil
+		}
+
+		backoff.Wait()
+	}
+
+	return nil, fmt.Errorf("failed to connect to kafka: %w", lastErr)
+}
+
+func sendMessages(producer sarama.SyncProducer, topic string, n int) error {
+	for i := range n {
+		_, _, err := producer.SendMessage(&sarama.ProducerMessage{
+			Topic: topic,
+			Value: sarama.StringEncoder("log line " + strconv.Itoa(i)),
+		})
+		if err != nil {
+			return fmt.Errorf("failed to send message to topic %q: %w", topic, err)
+		}
+	}
+
+	return nil
+}
