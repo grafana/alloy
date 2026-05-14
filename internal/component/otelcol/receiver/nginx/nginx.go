0c22f82f1fff43c9c6d9d8d34cc41b139486fadb
Mike Hennessy <henworth@henabytes.com>
2026-05-14 10:41:24 -0400

    chore: Update stability level and add otel link

--- internal/component/otelcol/receiver/nginx/nginx.go
+++ internal/component/otelcol/receiver/nginx/nginx.go
@@ -17,7 +17,7 @@ import (
 func init() {
 	component.Register(component.Registration{
 		Name:      "otelcol.receiver.nginx",
-		Stability: featuregate.StabilityExperimental,
+		Stability: featuregate.StabilityPublicPreview,
 		Args:      Arguments{},
 
 		Build: func(opts component.Options, args component.Arguments) (component.Component, error) {
