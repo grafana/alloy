2bdbd72547068c5a1918d6dbc8a9ff7b3a769854
Kalle <23356117+kalleep@users.noreply.github.com>
2026-06-02 15:17:14 +0200

    Hack slog to zap adapter

--- internal/slogadapter/zap.go
+++ internal/slogadapter/zap.go
@@ -4,9 +4,13 @@ import (
 	"log/slog"
 
 	"go.uber.org/zap"
+
+	"github.com/grafana/alloy/internal/util/zapadapter"
 )
 
-// FIXME(kalleep): zap adapter for slog
+// Zap returns a [zap.Logger] that writes to the provided slog.Logger.
 func Zap(l *slog.Logger) *zap.Logger {
-	return nil
+	// FIXME(kalleep): This is a hack, we use the go-kit logger adapter and pass
+	// it to zapadapter. We should implement proper zap to slog adapter.
+	return zapadapter.New(GoKit(l.Handler()))
 }
