grafana/alloy

commit ee2316252e6e0ee901b0ff57c55d6d07750d14ab

fix: Ensure the squid exporter wrapper properly brackets ipv6 addresses (#5199)

internal/static/integrations/squid_exporter/squid_exporter.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"net"
+	"net/netip"
 	"strconv"
 
 	se "github.com/boynux/squid-exporter/collector"
@@ -44,7 +45,13 @@ func (c *Config) validate() error {
 	if host == "" {
 		return ErrNoHostname
 	}
-	c.Host = host
+
+	if ip, err := netip.ParseAddr(host); err == nil && ip.IsValid() && ip.Is6() {
+		// Restore the explicit brackets for IPv6 addresses
+		c.Host = fmt.Sprintf("[%s]", host)
+	} else {
+		c.Host = host
+	}
 
 	if port == "" {
 		return ErrNoPort
@@ -95,7 +102,7 @@ func New(c *Config) (integrations.Integration, error) {
 	}
 
 	seExporter := se.New(&se.CollectorConfig{
-		Hostname: c.Host,
+		Hostname: fmt.Sprintf("[%s]", c.Host),
 		Port:     c.Port,
 		Login:    c.Username,
 		Password: string(c.Password),

internal/static/integrations/squid_exporter/squid_exporter_test.go
@@ -12,9 +12,10 @@ import (
 
 func TestConfigValidate(t *testing.T) {
 	cases := []struct {
-		name        string
-		getConfig   func() Config
-		expectedErr error
+		name         string
+		getConfig    func() Config
+		expectedErr  error
+		expectedHost string
 	}{
 		{
 			name: "valid",
@@ -64,17 +65,30 @@ func TestConfigValidate(t *testing.T) {
 			},
 			expectedErr: errors.New("address a@#$%:asdf::12312: too many colons in address"),
 		},
+		{
+			name: "valid ipv6",
+			getConfig: func() Config {
+				cfg := Config{}
+				cfg.Address = "[::1]:51001"
+				return cfg
+			},
+			expectedHost: "[::1]",
+		},
 	}
 
 	for _, tc := range cases {
 		t.Run(tc.name, func(t *testing.T) {
 			cfg := tc.getConfig()
 			err := cfg.validate()
-			if tc.expectedErr == nil {
-				require.NoError(t, err)
+			if tc.expectedErr != nil {
+				require.ErrorContains(t, err, tc.expectedErr.Error())
 				return
 			}
-			require.ErrorContains(t, err, tc.expectedErr.Error())
+
+			require.NoError(t, err)
+			if tc.expectedHost != "" {
+				require.Equal(t, tc.expectedHost, cfg.Host)
+			}
 		})
 	}
 }

commit 4259b8ff6dc3fe46fa2eee6b9a501f0fc64eb93b

fix: Remove extra bracketing of squid exporter host (#6573)
<!--
  CONTRIBUTORS GUIDE:

https://github.com/grafana/alloy/blob/main/docs/developer/contributing.md

If this is your first PR or you have not contributed in a while, we
recommend
  taking the time to review the guide.

  **NOTE**
Your PR title must adhere to Conventional Commit style with a
capitalized
description, e.g. `feat(scope): Add the thing` (not `add the thing`).
For
  details on this, check out the Contributors Guide linked above.
-->

### Brief description of Pull Request
Fix extra bracketing of squid exporter host field.
<!-- Human-readable description of the PR used as squash commit body.
-->


### Pull Request Details
After https://github.com/grafana/alloy/pull/6554 was merged, behavior
change in the squid exporter exposed an issue introduced by
https://github.com/grafana/alloy/pull/5199.
<!-- Detailed descripion of the Pull Request, if needed. -->


### Issue(s) fixed by this Pull Request

<!-- Fixes #issue_id -->


### Notes to the Reviewer

<!-- Relevant notes for reviewers/testers. -->


### PR Checklist

<!-- Remove items that do not apply. For completed items, change [ ] to
[x]. -->

- [ ] Documentation added
- [ ] Tests updated
- [ ] Config converters updated

internal/static/integrations/squid_exporter/squid_exporter.go
@@ -101,7 +101,7 @@ func New(c *Config) (integrations.Integration, error) {
 	}
 
 	seExporter := se.New(&se.CollectorConfig{
-		Hostname: fmt.Sprintf("[%s]", c.Host),
+		Hostname: c.Host,
 		Port:     c.Port,
 		Login:    c.Username,
 		Password: string(c.Password),