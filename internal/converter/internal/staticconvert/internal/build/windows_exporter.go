grafana/alloy

commit 0365ee238d842d3b3cca47c48bace33f303a6842

chore: Fix windows exporter's static converter (#6555)

internal/converter/internal/staticconvert/internal/build/windows_exporter.go
@@ -131,6 +131,9 @@ func toWindowsExporter(config *windows_exporter.Config) *windows.Arguments {
 		TCP: windows.TCPConfig{
 			EnabledList: split(config.TCP.EnabledList),
 		},
+		Time: windows.TimeConfig{
+			EnabledList: split(config.Time.EnabledList),
+		},
 		DNS: windows.DNSConfig{
 			EnabledList: split(config.DNS.EnabledList),
 		},

commit a97ec91d4ae98964b744ecc78a77bcd053530f07

fix: Remove duplicate Time field in windows exporter (#6633)
### Brief description of Pull Request

Remove duplicated time block in the windows exporter converter

### Notes to the Reviewer

This broke with #6555 which looks like it was merged after #6596 landed
with the same change. I think github could resolve it so it didn't come
through as a breaking change.

internal/converter/internal/staticconvert/internal/build/windows_exporter.go
@@ -146,8 +146,5 @@ func toWindowsExporter(config *windows_exporter.Config) *windows.Arguments {
 			Exclude:     config.Net.Exclude,
 			EnabledList: split(config.Net.EnabledList),
 		},
-		Time: windows.TimeConfig{
-			EnabledList: split(config.Time.EnabledList),
-		},
 	}
 }