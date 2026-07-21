1a9ed956fa34a172b97b0d11c72d2e9232f359f2
Gergely Madarász <gergely.madarasz@tulip.co>
2026-07-21 20:08:05 +0200

    feat: Add start_timestamp_zero_ingestion to Prometheus metric components (#6610)

--- internal/component/prometheus/operator/common/component.go
+++ internal/component/prometheus/operator/common/component.go
@@ -129,6 +129,10 @@ func (c *Component) Update(args component.Arguments) error {
 		return fmt.Errorf("enable_type_and_unit_labels is an experimental feature, and must be enabled by setting the stability.level flag to experimental")
 	}
 
+	if cfg.Scrape.StartTimestampZeroIngestion && !c.opts.MinStability.Permits(featuregate.StabilityExperimental) {
+		return fmt.Errorf("start_timestamp_zero_ingestion is an experimental feature, and must be enabled by setting the stability.level flag to experimental")
+	}
+
 	select {
 	case c.onUpdate <- struct{}{}:
 	default:
