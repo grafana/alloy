6514f9c6d883b636271f5f79e1a914f80c31076d
Kalle <23356117+kalleep@users.noreply.github.com>
2026-08-18 15:05:36 +0200

    fix error message

--- internal/component/loki/process/stages/pipeline_test.go
+++ internal/component/loki/process/stages/pipeline_test.go
@@ -95,6 +95,9 @@ func runPipelineTest(t *testing.T, cfgs []StageConfig, entries []Entry, expected
 		registry := prometheus.NewRegistry()
 		p, err := NewPipeline(logging.NewSlogNop(), cfgs, registry, featuregate.StabilityGenerallyAvailable)
 		require.NoError(t, err)
+		defer p.Cleanup()
+		defer p.Stop()
+
 		out := p.Run(withInboundEntries(cloned...))
 		var collected []Entry
 		for e := range out {
@@ -117,9 +120,9 @@ func runPipelineTest(t *testing.T, cfgs []StageConfig, entries []Entry, expected
 
 		p, err := newPipeline(logging.NewSlogNop(), registry, featuregate.StabilityGenerallyAvailable, cfgs, next)
 		require.NoError(t, err)
+		defer p.stop()
 
 		p.process(context.Background(), entries)
-		p.stop()
 
 		require.EventuallyWithT(t, func(c *assert.CollectT) {
 			assertEntriesUnordered(c, expected, collected)
@@ -257,7 +260,7 @@ func assertEntriesUnordered(t require.TestingT, expected, actual []Entry) {
 			}
 		}
 
-		require.NotEqual(t, -1, found)
+		require.NotEqual(t, -1, found, "no matching entry found for expected entry: %+v", exp)
 		remaining = append(remaining[:found], remaining[found+1:]...)
 	}
 }
