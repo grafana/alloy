829aef7fed0b153d2a7bccecfe45d674ecdf3339
Kalle <23356117+kalleep@users.noreply.github.com>
2026-08-18 15:05:36 +0200

    fix error message

--- internal/component/loki/process/stages/pipeline_test.go
+++ internal/component/loki/process/stages/pipeline_test.go
@@ -95,6 +95,9 @@ func runPipelineTest(t *testing.T, cfgs []StageConfig, entries []Entry, expected
 		registry := prometheus.NewRegistry()
 		p, err := NewPipeline(logging.NewSlogNop(), cfgs, registry, featuregate.StabilityGenerallyAvailable)
 		require.NoError(t, err)
+		defer p.Cleanup()
+		defer p.Stop()
+
 		out := p.Run(withInboundEntries(cloned...))
 		var collected []Entry
 		for e := range out {
@@ -255,7 +258,7 @@ func assertEntriesUnordered(t require.TestingT, expected, actual []Entry) {
 			}
 		}
 
-		require.NotEqual(t, -1, found)
+		require.NotEqual(t, -1, found, "no matching entry found for expected entry: %+v", exp)
 		remaining = append(remaining[:found], remaining[found+1:]...)
 	}
 }
