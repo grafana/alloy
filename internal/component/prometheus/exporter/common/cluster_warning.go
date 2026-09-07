1273c537151a4c626f6c398c9d2ad2e882cd89e5
Bejal Lewis <164711649+blewis12@users.noreply.github.com>
2026-09-07 15:47:47 +0200

    only warn if cluster enabled and cluster peers > 1

--- internal/component/prometheus/exporter/common/cluster_warning.go
+++ internal/component/prometheus/exporter/common/cluster_warning.go
@@ -18,7 +18,7 @@ func WarningIfUsedInCluster(o component.Options) {
 		return
 	}
 
-	if !clusterData.Ready() || len(clusterData.Peers()) > 0 {
+	if clusterData.Enabled() && len(clusterData.Peers()) > 1 {
 		o.Logger.Warn(
 			"detected clustering is configured while using a host-specific exporter - please make sure your configuration is correct",
 			"exporter",
