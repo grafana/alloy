8e4d562c26c672063d238c20b7a048272ca8f518
Karl Persson <23356117+kalleep@users.noreply.github.com>
2026-09-09 19:42:49 +0200

    chore(loki): Move created timestamp to stream instead of batch. (#7050)
    ### Pull Request Details
    
    From https://github.com/grafana/alloy/pull/6913#discussion_r3880197335.
    When passing entries through `loki.process` we need to rebuild the
    batch. Here we can get entries from other streams/batches we want to
    store the oldest created time we can find. To make this a bit more
    accurate I changed it so that we store created in every stream instead
    for every batch.
    
    ### Issue(s) fixed by this Pull Request
    
    <!-- Fixes #issue_id or leave empty if not applicable -->
    
    ### Notes to the Reviewer
    
    <!-- HUMAN ONLY: Context for reviewers/testers. Can leave empty if not
    needed. -->
    
    
    ### PR Checklist
    
    <!-- HUMAN ONLY: For completed items, change [ ] to [x]. -->
    
    - [ ] Documentation added
    - [x] Tests updated
    - [ ] Config converters updated
    - [ ] This pull request was substantially generated with AI assistance
    (see the [GenAI
    policy](https://github.com/grafana/alloy/blob/main/docs/developer/genai.md))

--- internal/component/common/loki/consumer_sharding.go
+++ internal/component/common/loki/consumer_sharding.go
@@ -58,8 +58,8 @@ func (s *ShardingConsumer) Consume(ctx context.Context, batch Batch) error {
 	if streamLen == 1 {
 		errChans = append(errChans, s.consume(ctx, s.shardFor(batch.streams[0].Labels), batch))
 	} else {
-		_ = batch.ConsumeStreams(func(stream Stream, created int64) error {
-			streamBatch := NewBatchWithCreatedUnixMicro(created)
+		_ = batch.ConsumeStreams(func(stream Stream) error {
+			streamBatch := NewBatch()
 			streamBatch.Add(stream)
 			errChans = append(errChans, s.consume(ctx, s.shardFor(stream.Labels), streamBatch))
 			return nil
