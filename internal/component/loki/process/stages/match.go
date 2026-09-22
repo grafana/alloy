baf557915d60cd2daae4d77734bc76f176020d6e
Kalle <23356117+kalleep@users.noreply.github.com>
2026-09-22 15:27:52 +0200

    lint

--- internal/component/loki/process/stages/match.go
+++ internal/component/loki/process/stages/match.go
@@ -307,7 +307,6 @@ func (m *matchKeepStage) collect(ctx context.Context, entries []Entry) error {
 	// In this case we should just set it to resulting entries from inner pipeline
 	// so that process can merged it back.
 	if buf, ok := fromMatchMerge(ctx); ok {
-
 		if *buf == nil {
 			*buf = entries
 		} else {
